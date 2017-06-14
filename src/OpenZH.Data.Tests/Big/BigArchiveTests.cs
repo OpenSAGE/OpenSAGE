d36bad2fdedfe7990dc4617da5556490194f168e
Tim Jones <tim@timjones.io>
2017-06-14 18:51:08 +0800

    Add BIG reader

---
+++ src/OpenZH.Data.Tests/Big/BigArchiveTests.cs
@@ -0,0 +1,66 @@
+using System.IO;
+using OpenZH.Data.Big;
+using Xunit;
+
+namespace OpenZH.Data.Tests.Big
+{
+    public class BigArchiveTests
+    {
+        private const string BigFilePath = @"C:\Program Files (x86)\Origin Games\Command and Conquer Generals Zero Hour\Command and Conquer Generals Zero Hour\W3DZH.big";
+
+        [Fact]
+        public void OpenBigArchive()
+        {
+            using (var bigStream = File.OpenRead(BigFilePath))
+            using (var bigArchive = new BigArchive(bigStream))
+            {
+                Assert.Equal(4432, bigArchive.Entries.Count);
+            }
+        }
+
+        [Fact]
+        public void ReadFirstEntry()
+        {
+            using (var bigStream = File.OpenRead(BigFilePath))
+            using (var bigArchive = new BigArchive(bigStream))
+            {
+                var firstEntry = bigArchive.Entries[0];
+
+                Assert.Equal(@"Art\W3D\ABBarracks_AC.W3D", firstEntry.FullName);
+                Assert.Equal(9334u, firstEntry.Length);
+            }
+        }
+
+        [Fact]
+        public void ReadFirstEntryStream()
+        {
+            using (var bigStream = File.OpenRead(BigFilePath))
+            using (var bigArchive = new BigArchive(bigStream))
+            {
+                var firstEntry = bigArchive.Entries[0];
+
+                using (var firstEntryStream = firstEntry.Open())
+                {
+                    Assert.Equal(9334, firstEntryStream.Length);
+
+                    var buffer = new byte[firstEntryStream.Length];
+                    var readBytes = firstEntryStream.Read(buffer, 0, 10000);
+                    Assert.Equal(9334, readBytes);
+                }
+            }
+        }
+
+        [Fact]
+        public void GetEntryByName()
+        {
+            using (var bigStream = File.OpenRead(BigFilePath))
+            using (var bigArchive = new BigArchive(bigStream))
+            {
+                var entry = bigArchive.GetEntry(@"Art\W3D\ABBarracks_AC.W3D");
+
+                Assert.Equal(@"Art\W3D\ABBarracks_AC.W3D", entry.FullName);
+                Assert.Equal(9334u, entry.Length);
+            }
+        }
+    }
+}
---
+++ src/OpenZH.Data/Utilities/Extensions/BinaryReaderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace OpenZH.Data.Utilities.Extensions
+{
+    internal static class BinaryReaderExtensions
+    {
+        public static uint ReadBigEndianUInt32(this BinaryReader reader)
+        {
+            var array = reader.ReadBytes(4);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(array);
+            return BitConverter.ToUInt32(array, 0);
+        }
+
+        public static string ReadNullTerminatedString(this BinaryReader reader)
+        {
+            var sb = new StringBuilder();
+
+            char c;
+            while ((c = reader.ReadChar()) != '\0')
+            {
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
