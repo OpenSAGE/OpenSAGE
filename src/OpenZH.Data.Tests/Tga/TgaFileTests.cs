21cbe625d39865acfafeab15b06e4a037b612f2c
Tim Jones <tim@timjones.io>
2017-06-28 22:38:22 +0800

    Add TGA parser

---
+++ src/OpenZH.Data.Tests/Tga/TgaFileTests.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Linq;
+using OpenZH.Data.Big;
+using OpenZH.Data.Tga;
+using Xunit;
+
+namespace OpenZH.Data.Tests.Tga
+{
+    public class TgaFileTests
+    {
+        private const string BigFilePath = @"C:\Program Files (x86)\Origin Games\Command and Conquer Generals Zero Hour\Command and Conquer Generals Zero Hour\TexturesZH.big";
+
+        [Fact]
+        public void CanReadTgaTextures()
+        {
+            using (var bigStream = File.OpenRead(BigFilePath))
+            using (var bigArchive = new BigArchive(bigStream))
+            {
+                foreach (var entry in bigArchive.Entries.Where(x => Path.GetExtension(x.FullName).ToLowerInvariant() == ".tga"))
+                {
+                    using (var entryStream = entry.Open())
+                    using (var binaryReader = new BinaryReader(entryStream))
+                    {
+                        var tgaFile = TgaFile.Parse(binaryReader);
+
+                        Assert.True(tgaFile.Header.ImagePixelSize == 24 || tgaFile.Header.ImagePixelSize == 32);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/OpenZH.Data/Tga/TgaFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace OpenZH.Data.Tga
+{
+    public sealed class TgaFile
+    {
+        public TgaHeader Header { get; private set; }
+        public byte[] Data { get; private set; }
+
+        public static TgaFile Parse(BinaryReader reader)
+        {
+            var header = TgaHeader.Parse(reader);
+
+            if (header.HasColorMap || header.ImageType != TgaImageType.UncompressedRgb)
+                throw new NotSupportedException();
+
+            var data = reader.ReadBytes(header.Width * header.Height * header.ImagePixelSize);
+
+            return new TgaFile
+            {
+                Header = header,
+                Data = data
+            };
+        }
+    }
+}
