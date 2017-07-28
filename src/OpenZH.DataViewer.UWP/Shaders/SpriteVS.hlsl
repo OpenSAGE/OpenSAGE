e99295e83acfc39cec8c3ca7b8174d048216d60d
Tim Jones <tim@timjones.io>
2017-07-28 20:05:18 +0800

    Attempt to fix sprite rendering

--- src/OpenZH.DataViewer.UWP/Shaders/SpriteVS.hlsl
+++ src/OpenZH.DataViewer.UWP/Shaders/SpriteVS.hlsl
@@ -3,6 +3,7 @@
 PSInput main(uint vertexID : SV_VERTEXID)
 {
 	PSInput output;
+  // Texture coordinates range [0, 2], but only [0, 1] appears on screen.
 	output.TexCoords = float2((vertexID << 1) & 2, vertexID & 2);
 	output.Position = float4(output.TexCoords * float2(2, -2) + float2(-1, 1), 0, 1);
 	output.Position.xy *= 0.9;
