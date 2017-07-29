4d020d9bc7a1708cd415b2aaeb9052a9d8bdfa0b
Tim Jones <tim@timjones.io>
2017-07-29 15:01:08 +0800

    Add initial Mesh class

---
+++ src/OpenZH.Game/Graphics/Mesh.cs
@@ -0,0 +1,93 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+using OpenZH.Data.W3d;
+using OpenZH.Game.Util;
+using OpenZH.Graphics;
+
+namespace OpenZH.Game.Graphics
+{
+    public sealed class Mesh : GraphicsObject
+    {
+        private readonly W3dMesh _w3dMesh;
+
+        private uint _numVertices;
+        private Buffer _vertexBuffer;
+
+        private uint _numIndices;
+        private Buffer _indexBuffer;
+
+        public Mesh(W3dMesh w3dMesh)
+        {
+            _w3dMesh = w3dMesh;
+        }
+
+        public void Initialize(GraphicsDevice graphicsDevice)
+        {
+            CreateVertexBuffer(graphicsDevice);
+
+            CreateIndexBuffer(graphicsDevice);
+        }
+
+        private void CreateVertexBuffer(GraphicsDevice graphicsDevice)
+        {
+            _numVertices = (uint) _w3dMesh.Vertices.Length;
+
+            var vertices = new MeshVertex[_numVertices];
+
+            for (var i = 0; i < _numVertices; i++)
+            {
+                vertices[i] = new MeshVertex
+                {
+                    Position = _w3dMesh.Vertices[i].ToVector3(),
+                    Normal = _w3dMesh.Normals[i].ToVector3(),
+                    UV = Vector2.Zero // TODO: _w3dMesh.MaterialPasses[0].TextureStages[0].TexCoords
+                };
+            }
+
+            var vertexBufferSize = _numVertices * MeshVertex.SizeInBytes;
+
+            _vertexBuffer = AddDisposable(new Buffer(graphicsDevice, vertexBufferSize));
+
+            _vertexBuffer.SetData(vertices, 0);
+        }
+
+        private void CreateIndexBuffer(GraphicsDevice graphicsDevice)
+        {
+            _numIndices = (uint) _w3dMesh.Triangles.Length * 3;
+
+            var indices = new ushort[_numIndices];
+            var indexIndex = 0;
+            foreach (var triangle in _w3dMesh.Triangles)
+            {
+                indices[indexIndex++] = (ushort) triangle.VIndex0;
+                indices[indexIndex++] = (ushort) triangle.VIndex1;
+                indices[indexIndex++] = (ushort) triangle.VIndex2;
+            }
+                                                                                                                    
+            _indexBuffer = AddDisposable(new Buffer(graphicsDevice, sizeof(ushort) * _numIndices));
+            _indexBuffer.SetData(indices, 0);
+        }
+
+        public void Draw(CommandEncoder commandEncoder)
+        {
+            commandEncoder.SetVertexBuffer(0, _vertexBuffer);
+
+            commandEncoder.DrawIndexed(
+                PrimitiveType.TriangleList,
+                _numIndices,
+                IndexType.UInt16,
+                _indexBuffer,
+                0);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MeshVertex
+        {
+            public const int SizeInBytes = sizeof(float) * 8;
+
+            public Vector3 Position;
+            public Vector3 Normal;
+            public Vector2 UV;
+        }
+    }
+}
---
+++ src/OpenZH.Game/Util/ConversionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+using OpenZH.Data.W3d;
+
+namespace OpenZH.Game.Util
+{
+    internal static class ConversionExtensions
+    {
+        public static Vector3 ToVector3(this W3dVector value)
+        {
+            return new Vector3(value.X, value.Y, value.Z);
+        }
+    }
+}
