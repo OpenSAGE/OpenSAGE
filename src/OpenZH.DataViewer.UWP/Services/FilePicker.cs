63653238d7fb8d1d3c77787251a73eaf84781cc0
Tim Jones <tim@timjones.io>
2017-07-30 12:42:04 +0800

    Store data path

--- src/OpenZH.DataViewer.UWP/Services/FilePicker.cs
+++ src/OpenZH.DataViewer.UWP/Services/FilePicker.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using OpenZH.DataViewer.Services;
 using OpenZH.DataViewer.UWP.Util;
 using Windows.ApplicationModel.Core;
+using Windows.Storage.AccessCache;
 using Windows.Storage.Pickers;
 
 namespace OpenZH.DataViewer.UWP.Services
@@ -14,10 +15,21 @@ namespace OpenZH.DataViewer.UWP.Services
             return CoreApplication.MainView.CoreWindow.Dispatcher.RunTaskAsync(
                 async () =>
                 {
+                    var futureAccessList = StorageApplicationPermissions.FutureAccessList;
+
+                    if (futureAccessList.Entries.Count > 0)
+                    {
+                        var entry = futureAccessList.Entries[0];
+                        var futureFolder = await futureAccessList.GetFolderAsync(entry.Token);
+                        return futureFolder.Path;
+                    }
+
                     var folderPicker = new FolderPicker();
                     folderPicker.FileTypeFilter.Add(".something"); // Otherwise PickSingleFolderAsync throws a COMException.
 
                     var pickedFolder = await folderPicker.PickSingleFolderAsync();
+
+                    futureAccessList.Add(pickedFolder);
 
                     return pickedFolder.Path;
                 });
