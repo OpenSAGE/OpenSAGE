034cee55002cb8ea68fce11a1a57383a1cc525c9
Tim Jones <tim@timjones.io>
2017-10-01 11:55:27 +0800

    Remove obsolete code

--- src/OpenSage.DataViewer/Framework/CameraController.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OpenSage.DataViewer.Framework
-{
-    public abstract class CameraController
-    {
-        public abstract void OnLeftMouseButtonDragged(float deltaX, float deltaY);
-        public abstract void OnMiddleMouseButtonDragged(float deltaY);
-        public abstract void OnRightMouseButtonDragged(float deltaX, float deltaY);
-    }
-}
---
+++ src/OpenSage.DataViewer/ViewModels/IGameViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenSage.DataViewer.ViewModels
+{
+    public interface IGameViewModel : IDisposable
+    {
+        Graphics.Cameras.Controllers.CameraController CameraController { get; }
+        void OnMouseMove(int x, int y);
+
+        Game Game { get; }
+    }
+}
