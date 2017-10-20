f23cc40ad8e6a41354fb65405d4a3a1d2982ee94
Paavo Huhtala <paavo.huhtala@gmail.com>
2017-10-21 01:23:11 +0300

    Set thread culture to InvariantCulture on startup
    Fixes the data viewer in locales which use the decimal comma.

--- src/OpenSage.DataViewer.Windows/App.xaml.cs
+++ src/OpenSage.DataViewer.Windows/App.xaml.cs
@@ -1,9 +1,17 @@
 ﻿using System.Windows;
+using System.Globalization;
+using System.Threading;
 
 namespace OpenSage.DataViewer
 {
     public partial class App : Application
     {
-        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+            base.OnStartup(e);
+        }
     }
 }
