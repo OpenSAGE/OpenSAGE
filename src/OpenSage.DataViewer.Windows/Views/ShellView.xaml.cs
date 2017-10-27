c5e9992fc969c751ae39f5d8834cfe9bc4a70b86
Tim Jones <tim@timjones.io>
2017-10-27 23:05:46 +0100

    Add search box

--- src/OpenSage.DataViewer.Windows/Views/ShellView.xaml.cs
+++ src/OpenSage.DataViewer.Windows/Views/ShellView.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using OpenSage.DataViewer.ViewModels;
 
 namespace OpenSage.DataViewer.Views
 {
@@ -7,6 +10,18 @@ namespace OpenSage.DataViewer.Views
         public ShellView()
         {
             InitializeComponent();
+        }
+
+        private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var sortedFiles = (CollectionViewSource) MainContent.FindResource("SortedFiles");
+            sortedFiles.View.Refresh();
+        }
+
+        private void OnFilteringSortedFiles(object sender, FilterEventArgs e)
+        {
+            var fileEntry = (FileSystemEntryViewModel) e.Item;
+            e.Accepted = fileEntry.FilePath.IndexOf(SearchBox.Text, StringComparison.OrdinalIgnoreCase) >= 0;
         }
     }
 }
