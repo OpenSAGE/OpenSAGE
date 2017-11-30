08b48ebeeaf3e7aa97ffbac1416fdf12e1ecaa08
Tim Jones <tim@timjones.io>
2017-12-01 03:17:32 +0800

    Add stub for .bik parser
    ffmpeg already has a .bik parser, but it adds a large native dependency, which will make things complicated when porting to iOS. I think it will be more straightforward in the long run to implement .bik parsing in-house, although I may be proved wrong...

---
+++ src/OpenSage.Game.Tests/Bik/BinkFileTests.cs
@@ -0,0 +1,33 @@
+﻿using OpenSage.Data.Bik;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OpenSage.Data.Tests.Bik
+{
+    public class BinkFileTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public BinkFileTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void CanReadBinkVideos()
+        {
+            InstalledFilesTestData.ReadFiles(".bik", _output, entry =>
+            {
+                using (var binkFile = BinkFile.FromFileSystemEntry(entry))
+                {
+                    Assert.NotNull(binkFile);
+
+                    for (var i = 0; i < binkFile.Header.NumFrames; i++)
+                    {
+                        var frame = binkFile.ReadFrame();
+                    }
+                }
+            });
+        }
+    }
+}
