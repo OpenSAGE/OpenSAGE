ba2b343e9ee4d0ab4d4ab29d5cfe63ede0e56ac0
Stephan Vedder <stephan.vedder@gmail.com>
2017-12-10 21:00:00 +0100

    Added apt constant data

---
+++ src/OpenSage.Game.Tests/Apt/ConstFileTests.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using OpenSage.Data.Big;
+using OpenSage.Data.Apt;
+using Xunit;
+using Xunit.Abstractions;
+using OpenSage.Data.Tests;
+using OpenSage.Data;
+
+namespace OpenSage.Game.Tests.Apt
+{
+    public class ConstFileTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public ConstFileTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+   
+        [Fact]
+        public void CanReadConstFiles()
+        {
+            var bigFilePath = Path.Combine(InstalledFilesTestData.GetInstallationDirectory(SageGame.BattleForMiddleEarthII), "apt/MainMenu.big");
+
+            using (var bigStream = File.OpenRead(bigFilePath))
+            using (var bigArchive = new BigArchive(bigStream))
+            {
+                var entry = bigArchive.GetEntry(@"MainMenu.const");
+
+                var data = ConstantData.FromFileSystemEntry(new FileSystemEntry(null, entry.FullName, entry.Length, entry.Open));
+                Assert.NotNull(data);
+            }
+        }
+    }
+}
---
+++ src/OpenSage.Game/Data/Apt/ConstantData.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenSage.Data.Utilities.Extensions;
+
+namespace OpenSage.Data.Apt
+{
+    public sealed class ConstantData
+    {
+        public List<ConstantEntry> Entries { get; internal set; }
+        public uint AptDataEntryOffset { get; internal set; }
+
+        public ConstantData()
+        {
+            Entries = new List<ConstantEntry>();
+        }
+
+        public static ConstantData FromFileSystemEntry(FileSystemEntry entry)
+        {
+            using (var stream = entry.Open())
+            using (var reader = new BinaryReader(stream))
+            {
+                var data = new ConstantData();
+
+                //validate that this is a correct const              
+                var magic = reader.ReadFixedLengthString(17);
+                if (magic!="Apt constant file")
+                    throw new InvalidDataException($"Not a supported const file: {magic}");
+
+                reader.BaseStream.Seek(3, SeekOrigin.Current);
+
+                data.AptDataEntryOffset = reader.ReadUInt32();
+                var numEntries = reader.ReadUInt32();
+
+                for(var i=0;i< numEntries;++i)
+                {
+                    var constEntry = new ConstantEntry
+                    {
+                        Type = (ConstantEntryType)reader.ReadUInt32()
+                    };
+
+                    //read the number/ string offset
+                    var entryValue = reader.ReadUInt32();
+
+                    switch (constEntry.Type)
+                    {
+                        case ConstantEntryType.UNDEF:
+                            throw new InvalidDataException("Undefined const entry");
+                        case ConstantEntryType.STRING:
+                            var pos = reader.BaseStream.Position;
+                            reader.BaseStream.Seek(entryValue, SeekOrigin.Begin);
+                            constEntry.Value =  reader.ReadNullTerminatedString();
+                            reader.BaseStream.Seek(pos, SeekOrigin.Begin);
+                            break;
+                        case ConstantEntryType.NUMBER:
+                            constEntry.Value = entryValue;
+                            break;
+                    }
+
+                    data.Entries.Add(constEntry);
+                }
+
+                return data;
+            }
+        }
+    }
+
+
+    public enum ConstantEntryType
+    {
+        UNDEF   = 0,
+        STRING  = 1,
+        NUMBER  = 4,
+    }
+
+    public sealed class ConstantEntry
+    {
+        public ConstantEntryType Type { get; internal set; }
+        public object Value { get; internal set; }
+    }
+}
