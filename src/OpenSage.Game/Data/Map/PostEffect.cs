8bcafd405830f6fb6cd7586938bceaaa7e89b593
Tim Jones <tim@timjones.io>
2017-12-31 00:55:07 +0800

    Parse PostEffect chunk (new in BFME II)

--- src/OpenSage.Game/Data/Map/PostEffect.cs
+++ src/OpenSage.Game/Data/Map/PostEffect.cs
@@ -1,17 +1,31 @@
 ﻿using System.IO;
+using OpenSage.Data.Utilities.Extensions;
 
 namespace OpenSage.Data.Map
 {
     public sealed class PostEffect
     {
+        public string Name { get; private set; }
+
+        // Only applicable to LookupTable?
+        public float BlendFactor { get; private set; }
+        public string LookupImage { get; private set; }
+
         internal static PostEffect Parse(BinaryReader reader)
         {
-            throw new System.NotImplementedException();
+            return new PostEffect
+            {
+                Name = reader.ReadUInt16PrefixedAsciiString(),
+                BlendFactor = reader.ReadSingle(),
+                LookupImage = reader.ReadUInt16PrefixedAsciiString()
+            };
         }
 
         internal void WriteTo(BinaryWriter writer)
         {
-            throw new System.NotImplementedException();
+            writer.WriteUInt16PrefixedAsciiString(Name);
+            writer.Write(BlendFactor);
+            writer.WriteUInt16PrefixedAsciiString(LookupImage);
         }
     }
 }
