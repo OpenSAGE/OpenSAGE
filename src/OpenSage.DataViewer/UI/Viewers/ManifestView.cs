3f94a779318bca64ddc29bce5e5b6d015d6bf0fe
Tim Jones <tim@timjones.io>
2018-01-09 23:32:10 +0800

    Add initial .manifest viewer

---
+++ src/OpenSage.DataViewer/UI/Viewers/ManifestView.cs
@@ -0,0 +1,38 @@
+﻿using Eto.Forms;
+using OpenSage.Data;
+using OpenSage.Data.StreamFS;
+
+namespace OpenSage.DataViewer.UI.Viewers
+{
+    public sealed class ManifestView : Splitter
+    {
+        public ManifestView(FileSystemEntry entry)
+        {
+            var gameStream = new GameStream(entry);
+
+            var rootTreeItem = new TreeItem();
+
+            var assetsTreeItem = new TreeItem { Text = "Assets" };
+            rootTreeItem.Children.Add(assetsTreeItem);
+            assetsTreeItem.Expanded = true;
+            foreach (var asset in gameStream.ManifestFile.Assets)
+            {
+                assetsTreeItem.Children.Add(new TreeItem
+                {
+                    Text = asset.Name,
+                    Tag = asset
+                });
+            }
+
+            var treeView = new TreeView
+            {
+                Width = 200,
+                DataStore = rootTreeItem
+            };
+            //listBox.SelectedValueChanged += (sender, e) => { };
+            Panel1 = treeView;
+
+            Panel2 = new Panel();
+        }
+    }
+}
