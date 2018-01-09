27a63960933f45d977dfc1f4efcaa27d0c98cb7c
Tim Jones <tim@timjones.io>
2018-01-09 23:30:57 +0800

    Add initial .manifest parser

---
+++ src/OpenSage.Game.Tests/StreamFS/ManifestFileTests.cs
@@ -0,0 +1,28 @@
+﻿using OpenSage.Data.StreamFS;
+using OpenSage.Data.Tests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OpenSage.Game.Tests.StreamFS
+{
+    public class ManifestFileTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public ManifestFileTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [GameFact(SageGame.Cnc3, SageGame.Cnc3KanesWrath)]
+        public void CanReadManifestFiles()
+        {
+            InstalledFilesTestData.ReadFiles(".manifest", _output, entry =>
+            {
+                var gameStream = new GameStream(entry);
+
+                Assert.NotNull(gameStream);
+            });
+        }
+    }
+}
---
+++ src/OpenSage.Game/Data/StreamFS/GameStream.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace OpenSage.Data.StreamFS
+{
+    public sealed class GameStream
+    {
+        public FileSystemEntry ManifestFileEntry { get; }
+        public ManifestFile ManifestFile { get; }
+
+        public GameStream(FileSystemEntry manifestFileEntry)
+        {
+            ManifestFileEntry = manifestFileEntry;
+            ManifestFile = ManifestFile.FromFileSystemEntry(manifestFileEntry);
+
+            ParseStreamFile(".bin", reader =>
+            {
+                foreach (var asset in ManifestFile.Assets)
+                {
+                    asset.InstanceData = reader.ReadBytes((int) asset.Header.InstanceDataSize);
+
+                    if (asset.Name.StartsWith("GameMap:"))
+                    {
+
+                    }
+                }
+            });
+
+            //ParseStreamFile(".relo", reader =>
+            //{
+            //    foreach (var asset in ManifestFile.Assets)
+            //    {
+            //        if (asset.Header.RelocationDataSize != 0)
+            //        {
+            //            uint reloValue;
+            //            do
+            //            {
+            //                reloValue = reader.ReadUInt32();
+
+            //                //asset.InstanceData[reloValue] += asset.InstanceData[0];
+            //            } while (reloValue != 0xFFFFFFFF);
+            //        }
+            //    }
+            //});
+        }
+
+        private void ParseStreamFile(string extension, Action<BinaryReader> callback)
+        {
+            var streamFilePath = Path.ChangeExtension(ManifestFileEntry.FilePath, extension);
+            var streamFileEntry = ManifestFileEntry.FileSystem.GetFile(streamFilePath);
+            if (streamFileEntry == null)
+            {
+                return;
+            }
+
+            using (var binaryStream = streamFileEntry.Open())
+            using (var reader = new BinaryReader(binaryStream, Encoding.ASCII, true))
+            {
+                var checksum = reader.ReadUInt32();
+                if (checksum != ManifestFile.Header.StreamChecksum)
+                {
+                    throw new InvalidDataException();
+                }
+
+                callback(reader);
+            }
+        }
+    }
+}
