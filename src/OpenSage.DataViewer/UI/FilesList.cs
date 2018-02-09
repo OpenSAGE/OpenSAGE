d1da5879b63e22c1133e5d11395a6295736b0cb6
Paavo Huhtala <paavo.huhtala@gmail.com>
2018-02-09 18:24:32 +0200

    Implement file export in data viewer

--- src/OpenSage.DataViewer/UI/FilesList.cs
+++ src/OpenSage.DataViewer/UI/FilesList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using Eto.Forms;
 using OpenSage.Data;
@@ -25,6 +26,11 @@ namespace OpenSage.DataViewer.UI
             _listBox = new ListBox();
             _listBox.ItemTextBinding = Binding.Property((FileSystemEntry e) => e.FilePath);
             _listBox.SelectedValueChanged += OnSelectedValueChanged;
+            _listBox.ContextMenu = new ContextMenu(new ButtonMenuItem(ExportSelectedItem)
+            {
+                Text = "Export..."
+            });
+
             Rows.Add(_listBox);
 
             mainForm.InstallationChanged += (sender, e) =>
@@ -33,6 +39,33 @@ namespace OpenSage.DataViewer.UI
 
                 RefreshItems();
             };
+        }
+
+        private void ExportSelectedItem(object sender, EventArgs args)
+        {
+            var entry = (FileSystemEntry) _listBox.SelectedValue;
+
+            var saveDialog = new SaveFileDialog();
+            saveDialog.FileName = Path.GetFileName(entry.FilePath);
+
+            var extension = Path.GetExtension(entry.FilePath);
+            saveDialog.Filters.Add(new FileDialogFilter($"{extension} Files", extension));
+            saveDialog.Filters.Add(new FileDialogFilter("All Files", "*.*"));
+
+            var result = saveDialog.ShowDialog(this);
+
+            if (result != DialogResult.Ok)
+            {
+                return;
+            }
+
+            using (var entryStream = entry.Open())
+            {
+                using (var fileStream = File.OpenWrite(saveDialog.FileName))
+                {
+                    entryStream.CopyTo(fileStream);
+                }
+            }
         }
 
         private void RefreshItems()
