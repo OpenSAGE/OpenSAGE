d0e7132da852248f56c45b94a398069f44e99209
Tim Jones <tim@timjones.io>
2018-04-02 15:12:20 +0800

    Share code to find all game installations

--- src/OpenSage.Viewer/Program.cs
+++ src/OpenSage.Viewer/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ImGuiNET;
+﻿using OpenSage.Viewer.UI;
 using Veldrid;
 
 namespace OpenSage.Viewer
@@ -8,6 +7,8 @@ namespace OpenSage.Viewer
     {
         public static void Main(string[] args)
         {
+            Platform.Start();
+
             using (var window = new GameWindow("OpenSAGE Viewer", 100, 100, 1024, 768))
             using (var commandList = window.GraphicsDevice.ResourceFactory.CreateCommandList())
             using (var imGuiRenderer = new ImGuiRenderer(window.GraphicsDevice, window.GraphicsDevice.MainSwapchain.Framebuffer.OutputDescription, 1024, 768))
@@ -19,6 +20,8 @@ namespace OpenSage.Viewer
                 };
 
                 gameTimer.Start();
+
+                var mainForm = new MainForm();
 
                 while (true)
                 {
@@ -39,23 +42,7 @@ namespace OpenSage.Viewer
                         (float) gameTimer.CurrentGameTime.ElapsedGameTime.TotalSeconds,
                         window.CurrentInputSnapshot);
 
-                    ImGui.BeginMainMenuBar();
-                    if (ImGui.BeginMenu("Help"))
-                    {
-                        if (ImGui.MenuItem("About", "Ctrl-Alt-A", false, true))
-                        {
-
-                        }
-                        ImGui.EndMenu();
-                    }
-                    ImGui.EndMainMenuBar();
-
-                    ImGui.BeginWindow("Test");
-
-                    ImGui.Text("Hello,");
-                    ImGui.Text("World!");
-
-                    ImGui.EndWindow();
+                    mainForm.Draw();
 
                     imGuiRenderer.Render(window.GraphicsDevice, commandList);
 
@@ -66,6 +53,8 @@ namespace OpenSage.Viewer
                     window.GraphicsDevice.SwapBuffers();
                 }
             }
+
+            Platform.Stop();
         }
     }
 }
---
+++ src/OpenSage.Viewer/UI/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using ImGuiNET;
+using OpenSage.Data;
+using OpenSage.Mods.BuiltIn;
+
+namespace OpenSage.Viewer.UI
+{
+    internal sealed class MainForm
+    {
+        public MainForm()
+        {
+            var installations = GameInstallation.FindAll(GameDefinition.All);
+        }
+
+        public void Draw()
+        {
+            ImGui.BeginMainMenuBar();
+            if (ImGui.BeginMenu("Installation"))
+            {
+                if (ImGui.MenuItem("About", "Ctrl-Alt-A", false, true))
+                {
+
+                }
+                ImGui.EndMenu();
+            }
+            ImGui.EndMainMenuBar();
+
+            ImGui.BeginWindow("Test");
+
+            ImGui.Text("Hello,");
+            ImGui.Text("World!");
+
+            ImGui.EndWindow();
+        }
+    }
+}
