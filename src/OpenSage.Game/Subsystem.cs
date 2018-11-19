c7b11467746a85b471d0a61f3cb9b1721bebcb32
Paavo Huhtala <paavo.huhtala@gmail.com>
2018-11-19 18:51:06 +0200

    Implement subsystem loaders, improve LoadSubstem parsing

---
+++ src/OpenSage.Game/Subsystem.cs
@@ -0,0 +1,44 @@
+﻿namespace OpenSage
+{
+    /// <summary>
+    /// Represents an abstract subsystem across different SAGE games.
+    /// Can map to multiple LoadSubsystem entries.
+    /// </summary>
+    public enum Subsystem
+    {
+        /// <summary>
+        /// Things that need to be loaded immediately on engine start, before loading any other assets.
+        /// </summary>
+        Core,
+
+        /// <summary>
+        /// Things that need to be loaded in order to load and spawn game objects.
+        /// </summary>
+        ObjectCreation,
+
+        /// <summary>
+        /// Things that need be loaded in order to spawn players or list them in menus.
+        /// </summary>
+        Players,
+
+        /// <summary>
+        /// Things that need to be loaded in order to load and render terrain and roads.
+        /// </summary>
+        Terrain,
+
+        /// <summary>
+        /// Things that need to be loaded in order to load and render particle systems.
+        /// </summary>
+        ParticleSystems,
+
+        /// <summary>
+        /// Things that need to be loaded in order to load and render Wnd-based GUIs.
+        /// </summary>
+        Wnd,
+
+        /// <summary>
+        /// Things that need to be loaded in order to start multiplayer (possibly including Skirmish).
+        /// </summary>
+        Multiplayer
+    }
+}
