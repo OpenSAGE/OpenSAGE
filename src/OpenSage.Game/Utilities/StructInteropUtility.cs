671cff335bc9e478b90de7a9ace1d8bbbf810113
Tim Jones <tim@timjones.io>
2018-11-24 21:42:09 +0000

    Remove unused method and simplify ToBytes implementation

--- src/OpenSage.Game/Utilities/StructInteropUtility.cs
+++ src/OpenSage.Game/Utilities/StructInteropUtility.cs
@@ -10,47 +10,7 @@ namespace OpenSage.Utilities
             var size = Marshal.SizeOf<T>();
             var result = new byte[size];
 
-            var handle = default(GCHandle);
-            try
-            {
-                handle = GCHandle.Alloc(result, GCHandleType.Pinned);
-
-                Marshal.StructureToPtr(value, handle.AddrOfPinnedObject(), false);
-            }
-            finally
-            {
-                if (handle.IsAllocated)
-                {
-                    handle.Free();
-                }
-            }
-
-            return result;
-        }
-
-        public static byte[] ToBytes<T>(T[] values)
-        {
-            var size = Marshal.SizeOf<T>();
-            var result = new byte[values.Length * size];
-
-            var handle = default(GCHandle);
-            try
-            {
-                handle = GCHandle.Alloc(result, GCHandleType.Pinned);
-                var baseAddress = handle.AddrOfPinnedObject();
-
-                for (var i = 0; i < values.Length; i++)
-                {
-                    Marshal.StructureToPtr(values[i], baseAddress + i * size, false);
-                }
-            }
-            finally
-            {
-                if (handle.IsAllocated)
-                {
-                    handle.Free();
-                }
-            }
+            MemoryMarshal.Write(result, ref value);
 
             return result;
         }
