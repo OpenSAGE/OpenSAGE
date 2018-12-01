bda6cd5b7a8f1fe4bc62389479299918ab854e2d
Tarcontar <michael-schnabel@gmx.net>
2018-12-01 21:20:45 +0100

    reached gondorhordes.ini

---
+++ src/OpenSage.Game/Logic/Object/Update/FadeAndDieOrnamentUpdate.cs
@@ -0,0 +1,17 @@
+﻿using OpenSage.Data.Ini.Parser;
+
+namespace OpenSage.Logic.Object
+{
+    [AddedIn(SageGame.Bfme)]
+    public sealed class FadeAndDieOrnamentUpdateModuleData : UpdateModuleData
+    {
+        internal static FadeAndDieOrnamentUpdateModuleData Parse(IniParser parser) => parser.ParseBlock(FieldParseTable);
+
+        private static readonly IniParseTable<FadeAndDieOrnamentUpdateModuleData> FieldParseTable = new IniParseTable<FadeAndDieOrnamentUpdateModuleData>
+        {
+            { "Envelope", (parser, x) => x.Envelope = Envelope.Parse(parser) },
+        };
+
+        public Envelope Envelope { get; private set; }
+    }
+}
