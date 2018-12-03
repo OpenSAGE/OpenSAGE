10eff450b37beeab2cfc3fa69e5d64384ac1339f
Tim Jones <tim@timjones.io>
2018-12-03 23:08:27 +0000

    Try to match SAGE ini float parsing

--- src/OpenSage.Game/Data/Utilities/ParseUtility.cs
+++ src/OpenSage.Game/Data/Utilities/ParseUtility.cs
@@ -1,17 +1,30 @@
 ﻿using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace OpenSage.Data.Utilities
 {
     internal static class ParseUtility
     {
+        private static readonly Regex FloatRegex = new Regex("^([-+]?[0-9]*\\.?[0-9]+)", RegexOptions.Compiled);
+
         public static float ParseFloat(string s)
         {
+            s = ExtractFloat(s);
             return float.Parse(s, CultureInfo.InvariantCulture);
         }
 
         public static bool TryParseFloat(string s, out float result)
         {
+            s = ExtractFloat(s);
             return float.TryParse(s, NumberStyles.Float, CultureInfo.InvariantCulture, out result);
+        }
+
+        private static string ExtractFloat(string s)
+        {
+            var match = FloatRegex.Match(s);
+            return match.Success
+                ? match.Groups[1].Value
+                : string.Empty;
         }
 
         public static string ToInvariant(float number)
