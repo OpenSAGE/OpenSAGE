2c4e3796aa1dc7eb97271b830033817ceabad10c
Tarcontar <michael-schnabel@gmx.net>
2018-12-03 23:14:19 +0100

    finished cinematiclocomotors.ini

---
+++ src/OpenSage.Game/Logic/Object/Die/DamageFilteredCreateObjectDie.cs
@@ -0,0 +1,25 @@
+﻿using OpenSage.Data.Ini;
+using OpenSage.Data.Ini.Parser;
+
+namespace OpenSage.Logic.Object
+{
+    [AddedIn(SageGame.Bfme2)]
+    public sealed class DamageFilteredCreateObjectDieModuleData : DieModuleData
+    {
+        internal static DamageFilteredCreateObjectDieModuleData Parse(IniParser parser) => parser.ParseBlock(FieldParseTable);
+
+        private static new readonly IniParseTable<DamageFilteredCreateObjectDieModuleData> FieldParseTable = DieModuleData.FieldParseTable
+            .Concat(new IniParseTable<DamageFilteredCreateObjectDieModuleData>
+            {
+                { "DamageTypeTriggersInstantly", (parser, x) => x.DamageTypeTriggersInstantly = parser.ParseEnum<DamageType>() },
+                { "DamageTypeTriggersForDuration", (parser, x) => x.DamageTypeTriggersForDuration = parser.ParseEnum<DamageType>() },
+                { "PostFilterTriggeredDuration", (parser, x) => x.PostFilterTriggeredDuration = parser.ParseInteger() },
+                { "CreationList", (parser, x) => x.CreationList = parser.ParseAssetReference() },
+            });
+
+        public DamageType DamageTypeTriggersInstantly { get; private set; }
+        public DamageType DamageTypeTriggersForDuration { get; private set; }
+        public int PostFilterTriggeredDuration { get; private set; }
+        public string CreationList { get; private set; }
+    }
+}
