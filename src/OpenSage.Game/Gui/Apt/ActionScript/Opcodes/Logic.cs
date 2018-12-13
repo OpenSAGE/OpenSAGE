3ad3a93c1e7f6894db0a8853096b64fb86d5c8f2
lanyi <lanyi@ra3.moe>
2018-12-13 23:33:43 +0100

    Implemented StrictEqual, remved some my temporary code used for debugging

--- src/OpenSage.Game/Gui/Apt/ActionScript/Opcodes/Logic.cs
+++ src/OpenSage.Game/Gui/Apt/ActionScript/Opcodes/Logic.cs
@@ -17,6 +17,36 @@ namespace OpenSage.Gui.Apt.ActionScript.Opcodes
         }
     }
 
+    /// <summary>
+    /// Pop two values from stack and check them for equality. Does work with types. Result on stack
+    /// </summary>
+    public sealed class Equals2 : InstructionBase
+    {
+        public override InstructionType Type => InstructionType.Equals2;
+
+        public override void Execute(ActionContext context)
+        {
+            var a = context.Stack.Pop();
+            var b = context.Stack.Pop();
+            bool eq = a.Equals(b);
+            context.Stack.Push(Value.FromBoolean(eq));
+
+        }
+    }
+
+    /// <summary>
+    /// Pop two values from stack and check them for equality. Does work with strings. Result on stack
+    /// </summary>
+    public sealed class LessThan2 : InstructionBase
+    {
+        public override InstructionType Type => InstructionType.LessThan2;
+
+        public override void Execute(ActionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     //Pop two values A,B from stack and check if B is greater A (reverse stack order)
     public sealed class Greater : InstructionBase
     {
@@ -29,6 +59,7 @@ namespace OpenSage.Gui.Apt.ActionScript.Opcodes
     }
 
     //Pop two values A,B from stack and check if B is greater A (reverse stack order)
+    // ^ Did us forget to edit comment after having copied it?
     public sealed class BitwiseXOr : InstructionBase
     {
         public override InstructionType Type => InstructionType.BitwiseXOr;
@@ -40,6 +71,23 @@ namespace OpenSage.Gui.Apt.ActionScript.Opcodes
             var result = Value.FromInteger(a ^ b);
 
             context.Stack.Push(result);
+        }
+    }
+
+    /// <summary>
+    /// Pop two values from stack and compare the two values using the Strict Equality Comparison Algorithm.
+    /// Push the resulting Boolean value onto the stack.
+    /// </summary>
+    public sealed class StrictEquals : InstructionBase
+    {
+        public override InstructionType Type => InstructionType.Equals2;
+
+        public override void Execute(ActionContext context)
+        {
+            var a = context.Stack.Pop();
+            var b = context.Stack.Pop();
+
+            context.Stack.Push(Value.FromBoolean(a.StrictEquals(b)));
         }
     }
 }
