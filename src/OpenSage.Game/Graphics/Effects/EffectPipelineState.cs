b609fd1c5f6e90a68a6ac329d8557a8d8c739bf6
Tim Jones <tim@timjones.io>
2018-12-19 23:14:38 +0000

    Improve EffectPipelineState performance

--- src/OpenSage.Game/Graphics/Effects/EffectPipelineState.cs
+++ src/OpenSage.Game/Graphics/Effects/EffectPipelineState.cs
@@ -1,8 +1,9 @@
-﻿using Veldrid;
+﻿using System;
+using Veldrid;
 
 namespace OpenSage.Graphics.Effects
 {
-    public readonly struct EffectPipelineState
+    public readonly struct EffectPipelineState : IEquatable<EffectPipelineState>
     {
         public readonly RasterizerStateDescription RasterizerState;
 
@@ -15,10 +16,10 @@ namespace OpenSage.Graphics.Effects
         public readonly EffectPipelineStateHandle Handle;
 
         public EffectPipelineState(
-            RasterizerStateDescription rasterizerState,
-            DepthStencilStateDescription depthStencilState,
-            BlendStateDescription blendState,
-            OutputDescription outputDescription)
+            in RasterizerStateDescription rasterizerState,
+            in DepthStencilStateDescription depthStencilState,
+            in BlendStateDescription blendState,
+            in OutputDescription outputDescription)
         {
             RasterizerState = rasterizerState;
             DepthStencilState = depthStencilState;
@@ -28,6 +29,39 @@ namespace OpenSage.Graphics.Effects
 
             Handle = null;
             Handle = EffectPipelineStateFactory.GetHandle(this);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is EffectPipelineState && Equals((EffectPipelineState) obj);
+        }
+
+        public bool Equals(EffectPipelineState other)
+        {
+            return RasterizerState.Equals(other.RasterizerState) &&
+                   DepthStencilState.Equals(other.DepthStencilState) &&
+                   BlendState.Equals(other.BlendState) &&
+                   OutputDescription.Equals(other.OutputDescription);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 414621651;
+            hashCode = hashCode * -1521134295 + RasterizerState.GetHashCode();
+            hashCode = hashCode * -1521134295 + DepthStencilState.GetHashCode();
+            hashCode = hashCode * -1521134295 + BlendState.GetHashCode();
+            hashCode = hashCode * -1521134295 + OutputDescription.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(in EffectPipelineState state1, in EffectPipelineState state2)
+        {
+            return state1.Equals(state2);
+        }
+
+        public static bool operator !=(in EffectPipelineState state1, in EffectPipelineState state2)
+        {
+            return !(state1 == state2);
         }
     }
 }
