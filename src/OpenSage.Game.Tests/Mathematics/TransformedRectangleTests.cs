0278352e3981017e484dbfe17a08e7d0693f6fef
Paavo Huhtala <paavo.huhtala@gmail.com>
2018-12-30 23:38:09 +0200

    Rework building collision checking to use 2D rectangle-rectangle intersect

---
+++ src/OpenSage.Game.Tests/Mathematics/TransformedRectangleTests.cs
@@ -0,0 +1,40 @@
+﻿using OpenSage.Mathematics;
+using Xunit;
+
+namespace OpenSage.Tests.Mathematics
+{
+    public class TransformedRectangleTest
+    {
+        [Fact]
+        public void IdenticalRectanglesIntersect()
+        {
+            var a = TransformedRectangle.FromRectangle(new RectangleF(0, 0, 10, 10));
+            var b = a;
+            Assert.True(a.Intersects(b));
+        }
+
+        [Fact]
+        public void RotatedRectanglesIntersect()
+        {
+            var a = TransformedRectangle.FromRectangle(new RectangleF(0, 0, 10, 10), 3.2f);
+            var b = TransformedRectangle.FromRectangle(new RectangleF(0, 0, 10, 10), 1.2f);
+            Assert.True(a.Intersects(b));
+        }
+
+        [Fact]
+        public void LargerRectangleContainsSmallerOne()
+        {
+            var a = TransformedRectangle.FromRectangle(new RectangleF(0, 0, 10, 10));
+            var b = TransformedRectangle.FromRectangle(new RectangleF(-5, -5, 15, 15));
+            Assert.True(a.Intersects(b));
+        }
+
+        [Fact]
+        public void CompletelySeparateRectanglesDontIntersect()
+        {
+            var a = TransformedRectangle.FromRectangle(new RectangleF(0, 0, 1, 1));
+            var b = TransformedRectangle.FromRectangle(new RectangleF(2, 0, 1, 1));
+            Assert.False(a.Intersects(b));
+        }
+    }
+}
---
+++ src/OpenSage.Game.Tests/Mathematics/Vector2UtilTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Numerics;
+using OpenSage.Mathematics;
+using Xunit;
+
+namespace OpenSage.Tests.Mathematics
+{
+    public class Vector2UtilityTests
+    {
+        [Fact]
+        public void RotationByZeroIsIdentity()
+        {
+            var axis = Vector2.Zero;
+            var point = new Vector2(1, 0);
+            var rotated = Vector2Utility.RotateAroundPoint(axis, point, 0);
+            VectorsEqual(point, rotated, 3);
+        }
+
+        [Fact]
+        public void RotationBy2PiIsIdentity()
+        {
+            var axis = Vector2.Zero;
+            var point = new Vector2(30, 30);
+            var rotated = Vector2Utility.RotateAroundPoint(axis, point, MathUtility.Pi * 2);
+            VectorsEqual(point, rotated, 3);
+        }
+
+        [Fact]
+        public void RotationAroundItselfIsIdentity()
+        {
+            var point = new Vector2(2, 2);
+            var rotated = Vector2Utility.RotateAroundPoint(point, point, MathUtility.Pi / 2);
+            VectorsEqual(point, rotated, 3);
+        }
+
+        [Fact]
+        public void RotationForwardsAndBackwardsIsIdentity()
+        {
+            var axis = Vector2.Zero;
+            var point = new Vector2(1, 3);
+
+            var rotatedCw = Vector2Utility.RotateAroundPoint(axis, point, 0.3f);
+            var rotatedCcw = Vector2Utility.RotateAroundPoint(axis, rotatedCw, -0.3f);
+            VectorsEqual(point, rotatedCcw, 3);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0, 1, 1.5708f, -1, 0)] // 90 degrees clockwise
+        [InlineData(0, 0, 0, 1, -1.5708f, 1, 0)] // 90 degrees counter-clockwise
+        public void RotationTheory(float axisX, float axisY, float pointX, float pointY, float angle, float resultX, float resultY)
+        {
+            var axis = new Vector2(axisX, axisY);
+            var point = new Vector2(pointX, pointY);
+            var expected = new Vector2(resultX, resultY);
+            var rotated = Vector2Utility.RotateAroundPoint(axis, point, angle);
+            VectorsEqual(expected, rotated, 3);
+        }
+
+        private static void VectorsEqual(Vector2 a, Vector2 b, int precision)
+        {
+            Assert.Equal(a.X, b.X, precision);
+            Assert.Equal(a.Y, b.Y, precision);
+        }
+    }
+}
