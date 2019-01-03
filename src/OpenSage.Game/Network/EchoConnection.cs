98a4254da31e10fe5b1cf8b4efca7bb177cfad2c
Tim Jones <tim@timjones.io>
2019-01-03 00:01:22 +0000

    Always ignore SetCameraPosition order

--- src/OpenSage.Game/Network/EchoConnection.cs
+++ src/OpenSage.Game/Network/EchoConnection.cs
@@ -14,8 +14,6 @@ namespace OpenSage.Network
 
         private readonly List<ReceivedPacket> _receivedPackets = new List<ReceivedPacket>();
 
-        public bool IsReplay { get; } = false;
-
         public void Send(uint frame, List<Order> orders)
         {
             _receivedPackets.Add(new ReceivedPacket
