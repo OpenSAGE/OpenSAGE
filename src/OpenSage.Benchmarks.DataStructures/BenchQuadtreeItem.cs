e72174fcaa2f1c2d6b6b40abdfa75c6a80780fe8
Paavo Huhtala <paavo.huhtala@gmail.com>
2019-01-07 00:24:17 +0200

    Implement more benchmarks for quadtree

---
+++ src/OpenSage.Benchmarks.DataStructures/BenchQuadtreeItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenSage.DataStructures;
+using OpenSage.Mathematics;
+
+namespace OpenSage.Benchmarks.DataStructures
+{
+    public class BenchQuadtreeItem : IHasBounds
+    {
+        public readonly int Id;
+        public RectangleF Bounds { get; set; }
+
+        public BenchQuadtreeItem(int id, RectangleF bounds)
+        {
+            Id = id;
+            Bounds = bounds;
+        }
+
+        public static BenchQuadtreeItem Generate(int id, RectangleF bounds, SizeF maxSize, Random random)
+        {
+            var x = (float) random.NextDouble() * bounds.Width - bounds.Left;
+            var y = (float) random.NextDouble() * bounds.Height - bounds.Top;
+
+            var width = MathF.Min(bounds.Right - x, (float) (random.NextDouble() * maxSize.Width));
+            var height = MathF.Min(bounds.Bottom - y, (float) (random.NextDouble() * maxSize.Height));
+
+            return new BenchQuadtreeItem(id, new RectangleF(x, y, width, height));
+        }
+
+        public BenchQuadtreeItem Clone()
+        {
+            return (BenchQuadtreeItem) MemberwiseClone();
+        }
+    }
+}
---
+++ src/OpenSage.Benchmarks.DataStructures/QuadtreeQuery.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using OpenSage.DataStructures;
+using OpenSage.Mathematics;
+
+namespace OpenSage.Benchmarks.DataStructures
+{
+    public class QuadtreeQuery
+    {
+        private readonly Random _random;
+        private Quadtree<BenchQuadtreeItem> _quadtree;
+        private BenchQuadtreeItem[] _items;
+
+        private RectangleF _queryArea;
+
+        [Params(3000)]
+        public int ItemCount { get; set; }
+
+        [Params(10f)]
+        public float ItemSize { get; set; }
+
+        [Params(100f)]
+        public float AreaSideLength { get; set; }
+
+        public QuadtreeQuery()
+        {
+            _random = new Random();
+        }
+
+        [GlobalSetup]
+        public void GenerateData()
+        {
+            _quadtree = new Quadtree<BenchQuadtreeItem>(new RectangleF(0, 0, 5000, 5000));
+            _items = new BenchQuadtreeItem[ItemCount];
+
+            for (var i = 0; i < ItemCount; i++)
+            {
+                var item = BenchQuadtreeItem.Generate(i, _quadtree.Bounds, new SizeF(ItemSize), _random);
+                _quadtree.Insert(item);
+                _items[i] = item;
+            }
+
+            // Since items are placed uniformly, it shouldn't matter which area this is.
+            _queryArea = new RectangleF(500, 500, AreaSideLength, AreaSideLength);
+        }
+
+        [Benchmark(Baseline = true)]
+        public List<BenchQuadtreeItem> QueryAreaArray()
+        {
+            var items = new List<BenchQuadtreeItem>();
+
+            foreach (var item in _items)
+            {
+                if (item.Bounds.IntersectsWith(_queryArea))
+                {
+                    items.Add(item);
+                }
+            }
+
+            return items;
+        }
+
+        [Benchmark]
+        public List<BenchQuadtreeItem> QueryAreaQuadtree()
+        {
+            return _quadtree.FindIntersecting(_queryArea).ToList();
+        }
+    }
+}
