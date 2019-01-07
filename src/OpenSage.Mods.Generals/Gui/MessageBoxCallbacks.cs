c95dcd7868bd4187c04989bf9d106cace124abc6
Stephan Vedder <stephan.vedder@gmail.com>
2019-01-07 23:21:25 +0100

    Remove unused code

--- src/OpenSage.Mods.Generals/Gui/MessageBoxCallbacks.cs
+++ src/OpenSage.Mods.Generals/Gui/MessageBoxCallbacks.cs
@@ -1,5 +1,4 @@
-﻿using OpenSage.Data.Ini;
-using OpenSage.Gui.Wnd;
+﻿using OpenSage.Gui.Wnd;
 using OpenSage.Gui.Wnd.Controls;
 
 namespace OpenSage.Mods.Generals.Gui
@@ -7,10 +6,6 @@ namespace OpenSage.Mods.Generals.Gui
     [WndCallbacks]
     public static class MessageBoxCallbacks
     {
-        private static Window _window;
-        private static Game _game;
-        private static MapCache _currentMap;
-
         public static void MessageBoxSystem(Control control, WndWindowMessage message, ControlCallbackContext context)
         {
             switch (message.MessageType)
