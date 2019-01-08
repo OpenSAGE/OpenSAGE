5f06ef6e5194ab833800db071f26aed932b500fb
Tarcontar <michael-schnabel@gmx.net>
2019-01-08 21:48:47 +0100

    all maps except nurn get loaded

--- src/OpenSage.Game/Logic/Object/Draw/W3dLightDraw.cs
+++ src/OpenSage.Game/Logic/Object/Draw/W3dLightDraw.cs
@@ -5,20 +5,20 @@ using OpenSage.Mathematics;
 namespace OpenSage.Logic.Object
 {
     [AddedIn(SageGame.Bfme)]
-    public class W3dLightDrawModuleData : W3dModelDrawModuleData
+    public class W3dLightDrawModuleData : DrawModuleData
     {
         internal static W3dLightDrawModuleData Parse(IniParser parser) => parser.ParseBlock(FieldParseTable);
 
-        internal static new readonly IniParseTable<W3dLightDrawModuleData> FieldParseTable = W3dModelDrawModuleData.FieldParseTable
-            .Concat(new IniParseTable<W3dLightDrawModuleData>
-            {
-                { "Ambient", (parser, x) => x.Ambient = parser.ParseColorRgba() },
-                { "Diffuse", (parser, x) => x.Diffuse = parser.ParseColorRgba() },
-                { "Radius", (parser, x) => x.Radius = parser.ParseInteger() },
-                { "Intensity", (parser, x) => x.Intensity = parser.ParseFloat() },
-                { "FlickerAmplitude", (parser, x) => x.FlickerAmplitude = parser.ParseFloat() },
-                { "FlickerFrequency", (parser, x) => x.FlickerFrequency = parser.ParseFloat() }
-            });
+        internal static readonly IniParseTable<W3dLightDrawModuleData> FieldParseTable = new IniParseTable<W3dLightDrawModuleData>
+        {
+            { "Ambient", (parser, x) => x.Ambient = parser.ParseColorRgba() },
+            { "Diffuse", (parser, x) => x.Diffuse = parser.ParseColorRgba() },
+            { "Radius", (parser, x) => x.Radius = parser.ParseInteger() },
+            { "Intensity", (parser, x) => x.Intensity = parser.ParseFloat() },
+            { "FlickerAmplitude", (parser, x) => x.FlickerAmplitude = parser.ParseFloat() },
+            { "FlickerFrequency", (parser, x) => x.FlickerFrequency = parser.ParseFloat() },
+            { "AttachToBoneInAnotherModule", (parser, x) => x.AttachToBoneInAnotherModule = parser.ParseBoneName() },
+        };
 
         public ColorRgba Ambient { get; private set; }
         public ColorRgba Diffuse { get; private set; }
@@ -26,5 +26,6 @@ namespace OpenSage.Logic.Object
         public float Intensity { get; private set; }
         public float FlickerAmplitude { get; private set; }
         public float FlickerFrequency { get; private set; }
+        public string AttachToBoneInAnotherModule { get; private set; }
     }
 }
