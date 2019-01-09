13220880bed274e17cb5aa8dffb2845ae62ef105
Tim Jones <tim@timjones.io>
2019-01-09 00:19:38 +0000

    Add wnd diagnostic window

---
+++ src/OpenSage.Game/Diagnostics/WndView.cs
@@ -0,0 +1,78 @@
+﻿using System.Numerics;
+using ImGuiNET;
+using OpenSage.Gui.Wnd.Controls;
+using OpenSage.Mathematics;
+
+namespace OpenSage.Diagnostics
+{
+    internal sealed class WndView : DiagnosticView
+    {
+        private Control _selectedControl;
+        private ColorRgbaF _originalBorderColor;
+        private int _originalBorderWidth;
+
+        public override string DisplayName { get; } = "WND Windows";
+
+        public WndView(DiagnosticViewContext context)
+            : base(context)
+        {
+            
+        }
+
+        protected override void DrawOverride(ref bool isGameViewFocused)
+        {
+            foreach (var wndWindow in Game.Scene2D.WndWindowManager.WindowStack)
+            {
+                if (ImGui.TreeNodeEx(wndWindow.DisplayName, ImGuiTreeNodeFlags.DefaultOpen | ImGuiTreeNodeFlags.OpenOnArrow))
+                {
+                    DrawControlTreeItemRecursive(wndWindow.Root);
+                }
+            }
+        }
+
+        private void DrawControlTreeItemRecursive(Control control)
+        {
+            var treeNodeFlags = ImGuiTreeNodeFlags.DefaultOpen | ImGuiTreeNodeFlags.OpenOnDoubleClick;
+            if (control == _selectedControl)
+            {
+                treeNodeFlags |= ImGuiTreeNodeFlags.Selected;
+            }
+
+            var opened = ImGui.TreeNodeEx(control.DisplayName, treeNodeFlags);
+            ImGuiUtility.DisplayTooltipOnHover(control.DisplayName);
+
+            if (ImGuiNative.igIsItemClicked(0) > 0)
+            {
+                SelectControl(control);
+            }
+
+            if (opened)
+            {
+                foreach (var child in control.Controls)
+                {
+                    DrawControlTreeItemRecursive(child);
+                }
+
+                ImGui.TreePop();
+            }
+        }
+
+        private void SelectControl(Control control)
+        {
+            if (_selectedControl != null)
+            {
+                _selectedControl.BorderColor = _originalBorderColor;
+                _selectedControl.BorderWidth = _originalBorderWidth;
+                _selectedControl = null;
+            }
+
+            _selectedControl = control;
+
+            _originalBorderColor = _selectedControl.BorderColor;
+            _originalBorderWidth = _selectedControl.BorderWidth;
+
+            _selectedControl.BorderColor = new ColorRgbaF(1f, 0.41f, 0.71f, 1);
+            _selectedControl.BorderWidth = 4;
+        }
+    }
+}
