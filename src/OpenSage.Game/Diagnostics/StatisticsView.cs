ad96b2f8b382fd716ec7b13d8651f0e69a332b9c
Tim Jones <tim@timjones.io>
2019-01-10 21:36:35 +0000

    Add (placeholder for) statistics view

---
+++ src/OpenSage.Game/Diagnostics/StatisticsView.cs
@@ -0,0 +1,21 @@
+﻿using ImGuiNET;
+
+namespace OpenSage.Diagnostics
+{
+    internal sealed class StatisticsView : DiagnosticView
+    {
+        public override string DisplayName { get; } = "Statistics";
+
+        public StatisticsView(DiagnosticViewContext context)
+            : base(context)
+        {
+
+        }
+
+        protected override void DrawOverride(ref bool isGameViewFocused)
+        {
+            ImGui.Text($"Rendered objects (opaque): {Context.Game.Graphics.RenderPipeline.RenderedObjectsOpaque}");
+            ImGui.Text($"Rendered objects (transparent): {Context.Game.Graphics.RenderPipeline.RenderedObjectsTransparent}");
+        }
+    }
+}
