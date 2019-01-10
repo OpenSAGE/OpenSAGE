1e21e051e1eb0d9ae5d209a97e1a34ebacea31d0
Tim Jones <tim@timjones.io>
2019-01-10 22:43:32 +0000

    Add search box (back) to strings view

--- src/OpenSage.Game/Diagnostics/StringsView.cs
+++ src/OpenSage.Game/Diagnostics/StringsView.cs
@@ -1,20 +1,37 @@
-﻿using ImGuiNET;
-using OpenSage.Content;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using ImGuiNET;
+using OpenSage.Data.Csf;
+using OpenSage.Diagnostics.Util;
 
 namespace OpenSage.Diagnostics
 {
     internal sealed class StringsView : DiagnosticView
     {
+        private readonly List<CsfLabel> _labels;
+        private readonly byte[] _searchTextBuffer;
+        private string _searchText;
+
         public override string DisplayName { get; } = "Strings";
 
         public StringsView(DiagnosticViewContext context)
             : base(context)
         {
-            
+            _labels = new List<CsfLabel>();
+            _searchTextBuffer = new byte[32];
         }
 
         protected override void DrawOverride(ref bool isGameViewFocused)
         {
+            ImGui.PushItemWidth(-1);
+            ImGuiUtility.InputText("##search", _searchTextBuffer, out var searchText);
+            UpdateSearch(searchText);
+            ImGui.PopItemWidth();
+
+            ImGui.BeginChild("strings", Vector2.Zero, false);
+
             ImGui.Columns(2, "CSF", true);
 
             ImGui.Separator();
@@ -22,7 +39,7 @@ namespace OpenSage.Diagnostics
             ImGui.Text("Value"); ImGui.NextColumn();
             ImGui.Separator();
 
-            foreach (var label in Game.ContentManager.TranslationManager.Labels)
+            foreach (var label in _labels)
             {
                 ImGui.Text(label.Name); ImGui.NextColumn();
 
@@ -37,6 +54,34 @@ namespace OpenSage.Diagnostics
             }
 
             ImGui.Columns(1, null, false);
+
+            ImGui.EndChild();
+        }
+
+        private void UpdateSearch(string searchText)
+        {
+            searchText = ImGuiUtility.TrimToNullByte(searchText);
+
+            if (searchText == _searchText)
+            {
+                return;
+            }
+
+            _searchText = searchText;
+
+            _labels.Clear();
+
+            foreach (var label in Context.Game.ContentManager.TranslationManager.Labels)
+            {
+                var matchesSearch =
+                    label.Name.Contains(searchText, StringComparison.OrdinalIgnoreCase)
+                    || label.Strings.Any(x => x.Value.Contains(searchText, StringComparison.OrdinalIgnoreCase));
+
+                if (matchesSearch)
+                {
+                    _labels.Add(label);
+                }
+            }
         }
 
         private static string CleanText(string text) => (text ?? string.Empty).Replace("%", "%%");
