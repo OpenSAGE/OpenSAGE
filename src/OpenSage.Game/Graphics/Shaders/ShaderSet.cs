fce90e98ddaba40a54873b6e20cc3a6b903f9c46
Tim Jones <tim@timjones.io>
2019-01-11 17:28:15 +0000

    Add ShaderSet and ShaderSetLibrary classes

---
+++ src/OpenSage.Game/Graphics/Shaders/ShaderSet.cs
@@ -0,0 +1,50 @@
+﻿using OpenSage.Utilities.Extensions;
+using Veldrid;
+using Veldrid.SPIRV;
+
+namespace OpenSage.Graphics.Shaders
+{
+    internal sealed class ShaderSet : DisposableBase
+    {
+        public readonly ShaderSetDescription Description;
+
+        public ShaderSet(
+            GraphicsDevice graphicsDevice,
+            string shaderName,
+            params VertexLayoutDescription[] vertexDescriptors)
+        {
+#if DEBUG
+            const bool debug = true;
+#else
+            const bool debug = false;
+#endif
+
+            byte[] ReadShader(string shaderType)
+            {
+                var bytecodeShaderName = $"OpenSage.Assets.Shaders.{shaderName}.{shaderType}.spv";
+                using (var shaderStream = typeof(ShaderSet).Assembly.GetManifestResourceStream(bytecodeShaderName))
+                {
+                    return shaderStream.ReadAllBytes();
+                }
+            }
+
+            var vsBytes = ReadShader("vert");
+            var fsBytes = ReadShader("frag");
+
+            var shaders = graphicsDevice.ResourceFactory.CreateFromSpirv(
+                new ShaderDescription(ShaderStages.Vertex, vsBytes, "main", debug),
+                new ShaderDescription(ShaderStages.Fragment, fsBytes, "main", debug),
+                new CrossCompileOptions());
+
+            var vertexShader = AddDisposable(shaders[0]);
+            var fragmentShader = AddDisposable(shaders[1]);
+
+            vertexShader.Name = $"{shaderName}.vert";
+            fragmentShader.Name = $"{shaderName}.frag";
+
+            Description = new ShaderSetDescription(
+                vertexDescriptors,
+                new[] { vertexShader, fragmentShader });
+        }
+    }
+}
