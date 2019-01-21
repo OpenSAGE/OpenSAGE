362147a9b260402bcfba6069ef78d3594e469028
Tim Jones <tim@timjones.io>
2019-01-21 21:45:03 +0000

    Add placeholders for missing window transitions

---
+++ src/OpenSage.Game/Gui/Wnd/Transitions/MainMenuScaleUpTransition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenSage.Gui.Wnd.Controls;
+
+namespace OpenSage.Gui.Wnd.Transitions
+{
+    internal sealed class MainMenuScaleUpTransition : WindowTransitionOperation
+    {
+        protected override int FrameDuration => 0; // TODO
+
+        public MainMenuScaleUpTransition(Control element, TimeSpan startTime)
+            : base(element, startTime)
+        {
+        }
+
+        protected override void OnUpdate(float progress)
+        {
+            // TODO
+        }
+    }
+}
---
+++ src/OpenSage.Game/Gui/Wnd/Transitions/TypeTextTransition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenSage.Gui.Wnd.Controls;
+
+namespace OpenSage.Gui.Wnd.Transitions
+{
+    internal sealed class TypeTextTransition : WindowTransitionOperation
+    {
+        protected override int FrameDuration => 0; // TODO
+
+        public TypeTextTransition(Control element, TimeSpan startTime)
+            : base(element, startTime)
+        {
+        }
+
+        protected override void OnUpdate(float progress)
+        {
+            // TODO
+        }
+    }
+}
