00b7366706111142528ef72300ca1af3ddde80ba
Tarcontar <michael-schnabel@gmx.net>
2019-01-26 18:44:23 +0100

    some changes

--- src/OpenSage.Game/Content/ContentManager.cs
+++ src/OpenSage.Game/Content/ContentManager.cs
@@ -129,7 +129,7 @@ namespace OpenSage.Content
                         SubsystemLoader.Load(Subsystem.ParticleSystems);
                         SubsystemLoader.Load(Subsystem.ObjectCreation);
                         SubsystemLoader.Load(Subsystem.Multiplayer);
-                        SubsystemLoader.Load(Subsystem.LinearCampaign);
+                        //SubsystemLoader.Load(Subsystem.LinearCampaign);
                         break;
                     default:
                         break;
