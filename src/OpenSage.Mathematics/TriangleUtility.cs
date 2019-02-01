280189f8e3282aff004cb8ba074ba62d0fb70fb7
Tarcontar <michael-schnabel@gmx.net>
2019-02-01 21:58:56 +0100

    fixed point triangle intersection

--- src/OpenSage.Mathematics/TriangleUtility.cs
+++ src/OpenSage.Mathematics/TriangleUtility.cs
@@ -5,37 +5,14 @@ namespace OpenSage.Mathematics
 {
     public static class TriangleUtility
     {
-        public static bool IsPointInside(in Vector2 v0, in Vector2 v1, in Vector2 v2, in Point2D point)
+        public static bool IsPointInside(in Vector2 v1, in Vector2 v2, in Vector2 v3, in Point2D point)
         {
-            var vPoint = new Vector2(point.X, point.Y);
-            var totalAngle = 0.0;
-            if (AngleBetweenPointAndLine(v0, v1, vPoint, ref totalAngle)
-                || AngleBetweenPointAndLine(v1, v2, vPoint, ref totalAngle)
-                || AngleBetweenPointAndLine(v2, v0, vPoint, ref totalAngle))
-            {
-                return true;
-            }
-
-            return (Math.Abs(totalAngle) > Constants.EPSILON) ? true : false;
-        }
-
-        private static bool AngleBetweenPointAndLine(in Vector2 start, in Vector2 end, in Vector2 point, ref double angle)
-        {
-            var crossProduct = Vector2Utility.CrossProductLength(start - point, point - point);
-            var dotProduct = Vector2.Dot(point - point, point - point);
-
-            if (PointIsBetweenPoints(dotProduct, crossProduct) || point.IsCongruentTo(point) || point.IsCongruentTo(point))
-            {
-                return true;
-            }
-            angle += Math.Atan2(crossProduct, dotProduct);
-            return false;
-        }
-
-
-        private static bool PointIsBetweenPoints(double dotProduct, double crossProduct)
-        {
-            return (Math.Abs(crossProduct) < Constants.EPSILON) && (dotProduct < 0.0);
+            var denominator = ((v2.Y - v3.Y)*(v1.X - v3.X) + (v3.X - v2.X)* (v1.Y - v3.Y));
+            var a = ((v2.Y - v3.Y) * (point.X - v3.X) + (v3.X - v2.X) * (point.Y - v3.Y)) / denominator;
+            var b = ((v3.Y - v1.Y) * (point.X - v3.X) + (v1.X - v3.X) * (point.Y - v3.Y)) / denominator;
+            var c = 1 - a - b;
+ 
+            return 0 <= a && a <= 1 && 0 <= b && b <= 1 && 0 <= c && c <= 1;
         }
     }
 }
