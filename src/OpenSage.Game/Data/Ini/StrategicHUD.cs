4a2204d2ce294c58fee519b898f01886dcdb347d
Tarcontar <michael-schnabel@gmx.net>
2019-02-05 18:58:25 +0100

    some work on strategichud

---
+++ src/OpenSage.Game/Data/Ini/StrategicHUD.cs
@@ -0,0 +1,29 @@
+﻿using OpenSage.Data.Ini.Parser;
+
+namespace OpenSage.Data.Ini
+{
+    [AddedIn(SageGame.Bfme2)]
+    public sealed class StrategicHUD
+    {
+        internal static StrategicHUD Parse(IniParser parser) => parser.ParseTopLevelBlock(FieldParseTable);
+
+        private static readonly IniParseTable<StrategicHUD> FieldParseTable = new IniParseTable<StrategicHUD>
+        {
+            { "ArmyDetailsPanel", (parser, x) => x.ArmyDetailsPanel = ArmyDetailsPanel.Parse(parser) },
+        };
+
+        public ArmyDetailsPanel ArmyDetailsPanel { get; private set; }
+    }
+
+    public class ArmyDetailsPanel
+    {
+        internal static ArmyDetailsPanel Parse(IniParser parser) => parser.ParseBlock(FieldParseTable);
+
+        private static readonly IniParseTable<ArmyDetailsPanel> FieldParseTable = new IniParseTable<ArmyDetailsPanel>
+        {
+            { "BackButtonTooltip", (parser, x) => x.BackButtonTooltip = parser.ParseLocalizedStringKey() },
+        };
+
+        public string BackButtonTooltip { get; private set; }
+    }
+}
