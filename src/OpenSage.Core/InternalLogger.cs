3669394636670304634d7927bd936c46ff408d20
Stephan Vedder <stephan.vedder@gmail.com>
2019-05-05 09:40:19 +0200

    Add ingame logger

---
+++ src/OpenSage.Core/InternalLogger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using NLog;
+using NLog.Targets;
+
+namespace OpenSage.Core
+{
+    [Target("OpenSage")]
+    public sealed class InternalLogger : TargetWithLayout
+    {
+        public struct Message
+        {
+            public string Content { get; set; }
+            public LogLevel Level { get; set; }
+        }
+
+        public List<Message> Messages { get; set; }
+
+        public InternalLogger()
+        {
+            Messages = new List<Message>();
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            string logMessage = this.Layout.Render(logEvent);
+
+            Messages.Add(new Message()
+            {
+                Content = logMessage,
+                Level = logEvent.Level
+            });
+        }
+    }
+}
---
+++ src/OpenSage.Game/Diagnostics/LogView.cs
@@ -0,0 +1,45 @@
+﻿using System.Numerics;
+using ImGuiNET;
+using NLog;
+using OpenSage.Core;
+
+namespace OpenSage.Diagnostics
+{
+    internal sealed class LogView : DiagnosticView
+    {
+        public LogView(DiagnosticViewContext context) : base(context)
+        {
+        }
+
+        public override string DisplayName => "Log View";
+
+        protected override void DrawOverride(ref bool isGameViewFocused)
+        {
+            var target = LogManager.Configuration.FindTargetByName("internal") as InternalLogger;
+
+            ImGui.BeginChild("Log", Vector2.Zero, false);
+
+            foreach (var msg in target.Messages)
+            {
+                var color = Vector4.One;
+
+                if(msg.Level == LogLevel.Warn)
+                {
+                    color = new Vector4(1, 1, 0, 1);
+                }
+                else if (msg.Level >= LogLevel.Error)
+                {
+                    color = new Vector4(1, 0, 0, 1);
+                }
+                else if (msg.Level == LogLevel.Info)
+                {
+                    color = new Vector4(0, 0, 1, 1);
+                }
+
+                ImGui.TextColored(color, msg.Content);
+            }
+
+            ImGui.EndChild();
+        }
+    }
+}
