8e9811169e244ef795b420f739ba5083971cd0d3
Tim Jones <tim@timjones.io>
2019-09-29 12:18:12 +0100

    Unify Generals and BFME Locomotor definitions

--- src/OpenSage.Game/Logic/Object/LocomotorSet.cs
+++ src/OpenSage.Game/Logic/Object/LocomotorSet.cs
@@ -1,4 +1,5 @@
-﻿using OpenSage.Data.Ini;
+﻿using OpenSage.Content;
+using OpenSage.Data.Ini;
 
 namespace OpenSage.Logic.Object
 {
@@ -11,13 +12,13 @@ namespace OpenSage.Logic.Object
 
         private static readonly IniParseTable<LocomotorSet> FieldParseTable = new IniParseTable<LocomotorSet>
         {
-            { "Locomotor", (parser, x) => x.Locomotor = parser.ParseString() },
+            { "Locomotor", (parser, x) => x.Locomotor = parser.ParseLocomotorTemplateReference() },
             { "Condition", (parser, x) => x.Condition = parser.ParseEnum<LocomotorSetCondition>() },
             { "Speed", (parser, x) => x.Speed = parser.ParseFloat() },
         };
 
-        public string Locomotor { get; private set; }
-        public LocomotorSetCondition Condition { get; private set; }
-        public float Speed { get; private set; }
+        public LazyAssetReference<LocomotorTemplate> Locomotor { get; internal set; }
+        public LocomotorSetCondition Condition { get; internal set; }
+        public float Speed { get; internal set; }
     }
 }
