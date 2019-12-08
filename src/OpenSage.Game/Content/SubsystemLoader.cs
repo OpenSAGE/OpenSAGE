a7c31e61fe4ea0fa324331c377bc28e816cf1148
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2019-12-08 16:46:21 +0100

    Moved parsing of MapCache.ini after UserDataFolder is known

--- src/OpenSage.Game/Content/SubsystemLoader.cs
+++ src/OpenSage.Game/Content/SubsystemLoader.cs
@@ -35,6 +35,7 @@ namespace OpenSage.Content
                         @"Data\INI\AudioSettings.ini",
                         $@"Data\{_contentManager.Language}\HeaderTemplate.ini",
                         @"Maps\MapCache.ini");
+
                     break;
                 case Subsystem.Audio:
                     LoadFiles(
@@ -77,9 +78,7 @@ namespace OpenSage.Content
                     _contentManager.LoadIniFiles(@"Data\INI\MappedImages\TextureSize_512\");
                     break;
                 case Subsystem.Multiplayer:
-                    LoadFiles(
-                        @"Data\INI\Multiplayer.ini");
-                    _contentManager.LoadIniFile(_contentManager.UserDataFileSystem.GetFile(@"Maps\MapCache.ini"));
+                    LoadFiles(@"Data\INI\Multiplayer.ini");
                     break;
                 case Subsystem.LinearCampaign:
                     LoadFiles(@"Data\INI\Campaign.ini");
