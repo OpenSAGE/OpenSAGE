2349a8d83f2c96070dfeb9fa2989024db8950b5a
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2019-12-08 16:24:34 +0100

    Load MapCache.ini from UserData folder

--- src/OpenSage.Game/Content/SubsystemLoader.cs
+++ src/OpenSage.Game/Content/SubsystemLoader.cs
@@ -77,7 +77,9 @@ namespace OpenSage.Content
                     _contentManager.LoadIniFiles(@"Data\INI\MappedImages\TextureSize_512\");
                     break;
                 case Subsystem.Multiplayer:
-                    LoadFiles(@"Data\INI\Multiplayer.ini");
+                    LoadFiles(
+                        @"Data\INI\Multiplayer.ini");
+                    _contentManager.LoadIniFile(_contentManager.UserDataFileSystem.GetFile(@"Maps\MapCache.ini"));
                     break;
                 case Subsystem.LinearCampaign:
                     LoadFiles(@"Data\INI\Campaign.ini");
