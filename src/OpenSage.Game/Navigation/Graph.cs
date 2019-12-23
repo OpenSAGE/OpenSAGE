f7cf2777ee406cb79a4ca4c6539d09f3815d6aa3
Stephan Vedder <stephan.vedder@gmail.com>
2019-12-23 22:31:49 +0100

    Work on custom pathfinder

---
+++ src/OpenSage.Game/Navigation/Graph.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenSage.Navigation
+{
+    class Graph
+    {
+        Node[] _nodes;
+    }
+}
---
+++ src/OpenSage.Game/Navigation/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace OpenSage.Navigation
+{
+    enum Passability
+    {
+        Passable,
+        Impassable,
+        ImpassableForTeams,
+        ImpassableForAir,
+    }
+
+    class Node
+    {
+        readonly int _x, _y;
+        Passability _passability;
+    }
+}
