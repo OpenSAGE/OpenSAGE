fadea9868d2ae675510019278de4c88bfde1dab6
Stephan Vedder <stephan.vedder@gmail.com>
2019-12-24 11:29:53 +0100

    Implement more graph logic

--- src/OpenSage.Game/Navigation/Node.cs
+++ src/OpenSage.Game/Navigation/Node.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Numerics;
-using System.Text;
+using System.Linq;
 
 namespace OpenSage.Navigation
 {
@@ -15,7 +14,51 @@ namespace OpenSage.Navigation
 
     class Node
     {
-        readonly int _x, _y;
-        Passability _passability;
+        private readonly int _x, _y;
+        private readonly Graph _graph;
+        public Passability Passability { get; set; }
+
+        internal Node(Graph graph, int x, int y)
+        {
+            _x = x;
+            _y = y;
+            _graph = graph;
+            Passability = Passability.Passable;
+        }
+
+        internal bool Passable()
+        {
+            return Passability == Passability.Passable;
+        }
+
+        internal int CalculateDistance(int targetX, int targetY)
+        {
+            return Math.Abs(targetX - _x) + Math.Abs(targetY - _y);
+        }
+
+        internal IEnumerable<Node> GetAdjacentPassableNodes()
+        {
+            return GetAdjacentNodes().Where(x => x.Passable());
+        }
+
+        internal IEnumerable<Node> GetAdjacentNodes()
+        {
+            if (_x > 0)
+            {
+                yield return _graph.GetNode(_x - 1, _y);
+            }
+            if (_y > 0)
+            {
+                yield return _graph.GetNode(_x, _y - 1);
+            }
+            if (_x < _graph.Width - 1)
+            {
+                yield return _graph.GetNode(_x + 1, _y);
+            }
+            if (_y < _graph.Height - 1)
+            {
+                yield return _graph.GetNode(_x, _y + 1);
+            }
+        }
     }
 }
