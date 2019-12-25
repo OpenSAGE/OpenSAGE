40c2c73e1b4914cd129ff0c77446b77e69ac86b2
Stephan Vedder <stephan.vedder@gmail.com>
2019-12-25 11:24:05 +0100

    Use a real priority queue

---
+++ src/OpenSage.Benchmarks.DataStructures/PathfindingQuery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using OpenSage.Navigation;
+
+namespace OpenSage.Benchmarks.DataStructures
+{
+    public class PathfindingQuery
+    {
+
+        Graph _graph;
+
+        public PathfindingQuery()
+        {
+            _graph = new Graph(1000, 1000);
+        }
+
+
+        [Benchmark]
+        public List<Node> DirectQuery()
+        {
+            var start = _graph.GetNode(10, 10);
+            var end = _graph.GetNode(990, 990);
+            return _graph.Search(start, end);
+        }
+
+    }
+}
