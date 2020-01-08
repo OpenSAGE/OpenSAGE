5b21e26621bbfe22192c5248f0fd0816fefce2a8
Tim Jones <tim@timjones.io>
2020-01-08 23:40:19 +0000

    Allow lower precision in angle tests

--- src/OpenSage.Game.Tests/Mathematics/AngleTests.cs
+++ src/OpenSage.Game.Tests/Mathematics/AngleTests.cs
@@ -6,16 +6,18 @@ namespace OpenSage.Tests.Mathematics
 {
     public class AngleTests
     {
+        private const int Precision = 6;
+
         [Fact]
         public void DegreesToRadiansTest()
         {
-            Assert.Equal(MathUtility.ToRadians(90.0f), MathUtility.PiOver2);
+            Assert.Equal(MathUtility.ToRadians(90.0f), MathUtility.PiOver2, Precision);
         }
 
         [Fact]
         public void RadiansToDegreesTest()
         {
-            Assert.Equal(90.0f, MathUtility.ToDegrees(MathUtility.PiOver2));
+            Assert.Equal(90.0f, MathUtility.ToDegrees(MathUtility.PiOver2), Precision);
         }
 
         [Theory]
@@ -25,7 +27,7 @@ namespace OpenSage.Tests.Mathematics
         [InlineData(-1.0f, 0.0f, 180.0f)]
         public void GetYawFromDirectionTest(float x, float y, float angle)
         {
-            Assert.Equal(angle, MathUtility.ToDegrees(MathUtility.GetYawFromDirection(new Vector2(x, y))));
+            Assert.Equal(angle, MathUtility.ToDegrees(MathUtility.GetYawFromDirection(new Vector2(x, y))), Precision);
         }
 
         [Theory]
@@ -35,7 +37,7 @@ namespace OpenSage.Tests.Mathematics
         public void GetPitchFromDirectionTest(float x, float y, float z, float angle)
         {
             var vec = Vector3.Normalize(new Vector3(x, y, z));
-            Assert.Equal(angle, MathUtility.ToDegrees(MathUtility.GetPitchFromDirection(vec)));
+            Assert.Equal(angle, MathUtility.ToDegrees(MathUtility.GetPitchFromDirection(vec)), Precision);
         }
 
         [Theory]
@@ -47,7 +49,7 @@ namespace OpenSage.Tests.Mathematics
             var alphaRad = MathUtility.ToRadians(alpha);
             var betaRad = MathUtility.ToRadians(beta);
             var deltaRad = MathUtility.ToRadians(delta);
-            Assert.Equal(deltaRad, MathUtility.CalculateAngleDelta(alphaRad, betaRad), 3);
+            Assert.Equal(deltaRad, MathUtility.CalculateAngleDelta(alphaRad, betaRad), Precision);
         }
     }
 }
