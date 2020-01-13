0c2ad179cb8f9a0978e722a127205ba78fbb3b8b
Tim Jones <tim@timjones.io>
2020-01-13 21:34:37 +0000

    Generate AttackGround order on ctrl-click

--- src/OpenSage.Game/Logic/Orders/OrderProcessor.cs
+++ src/OpenSage.Game/Logic/Orders/OrderProcessor.cs
@@ -130,7 +130,7 @@ namespace OpenSage.Logic.Orders
                             var targetPosition = order.Arguments[0].Value.Position;
                             foreach (var unit in player.SelectedUnits)
                             {
-                                //Attack the targetPosition
+                                unit.TargetEnemyPosition = targetPosition;
                             }
                         }
                         break;
