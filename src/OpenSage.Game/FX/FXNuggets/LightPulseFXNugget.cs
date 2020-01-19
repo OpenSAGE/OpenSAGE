ae53261d31b060bf109c7e141a23421d9fe89bde
Tim Jones <tim@timjones.io>
2020-01-19 21:07:49 +0000

    Some work on LightPulse nugget

--- src/OpenSage.Game/FX/FXNuggets/LightPulseFXNugget.cs
+++ src/OpenSage.Game/FX/FXNuggets/LightPulseFXNugget.cs
@@ -1,4 +1,5 @@
-﻿using OpenSage.Data.Ini;
+﻿using System;
+using OpenSage.Data.Ini;
 using OpenSage.Mathematics;
 
 namespace OpenSage.FX
@@ -12,14 +13,20 @@ namespace OpenSage.FX
             { "Color", (parser, x) => x.Color = parser.ParseColorRgb() },
             { "Radius", (parser, x) => x.Radius = parser.ParseInteger() },
             { "RadiusAsPercentOfObjectSize", (parser, x) => x.RadiusAsPercentOfObjectSize = parser.ParsePercentage() },
-            { "IncreaseTime", (parser, x) => x.IncreaseTime = parser.ParseInteger() },
-            { "DecreaseTime", (parser, x) => x.DecreaseTime = parser.ParseInteger() }
+            { "IncreaseTime", (parser, x) => x.IncreaseTime = parser.ParseTimeMilliseconds() },
+            { "DecreaseTime", (parser, x) => x.DecreaseTime = parser.ParseTimeMilliseconds() }
         });
 
         public ColorRgb Color { get; private set; }
-        public int Radius { get; private set; }
-        public Percentage RadiusAsPercentOfObjectSize { get; private set; }
-        public int IncreaseTime { get; private set; }
-        public int DecreaseTime { get; private set; }
+        public int? Radius { get; private set; }
+        public Percentage? RadiusAsPercentOfObjectSize { get; private set; }
+        public TimeSpan IncreaseTime { get; private set; }
+        public TimeSpan DecreaseTime { get; private set; }
+
+        internal override void Execute(FXListExecutionContext context)
+        {
+            // TODO: Add dynamic point light with specified colour and radius to scene,
+            // set to expire after IncreaseTime + DecreaseTime.
+        }
     }
 }
