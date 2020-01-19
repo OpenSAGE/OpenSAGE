e670eee675766267661d6efe36db6669ef596179
Tim Jones <tim@timjones.io>
2020-01-19 21:20:29 +0000

    Add FXList asset view

---
+++ src/OpenSage.Game/Diagnostics/AssetViews/FXListView.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+using OpenSage.FX;
+
+namespace OpenSage.Diagnostics.AssetViews
+{
+    [AssetView(typeof(FXList))]
+    internal sealed class FXListView : AssetView
+    {
+        private readonly RenderedView _renderedView;
+
+        public FXListView(DiagnosticViewContext context, FXList fxList)
+            : base(context)
+        {
+            _renderedView = AddDisposable(new RenderedView(context));
+
+            fxList.Execute(
+                new FXListExecutionContext(
+                    null, // TODO
+                    Matrix4x4.Identity,
+                    _renderedView.Scene.GameContext));
+        }
+
+        public override void Draw()
+        {
+            _renderedView.Draw();
+        }
+    }
+}
