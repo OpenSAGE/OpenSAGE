9534cad0efb95f41da8eafef04f2db7c5e961fde
Stephan Vedder <stephan.vedder@gmail.com>
2020-04-11 16:20:55 +0200

    More lobby progress

---
+++ src/OpenSage.Game/Network/LobbyProtocol.cs
@@ -0,0 +1,18 @@
+﻿using ProtoBuf;
+
+namespace OpenSage.Network
+{
+    internal class LobbyProtocol
+    {
+        [ProtoContract]
+        internal struct LobbyBroadcast
+        {
+            [ProtoMember(1)]
+            public string Name;
+            [ProtoMember(2)]
+            public string Map;
+            [ProtoMember(3)]
+            public int Players;
+        }
+    }
+}
