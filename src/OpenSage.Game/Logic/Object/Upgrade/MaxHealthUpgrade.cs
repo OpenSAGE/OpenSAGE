106db7f05eb8c51f9b66946c1a45061ea733f305
Stephan Vedder <stephan.vedder@gmail.com>
2020-07-18 15:02:20 +0200

    Implement max health upgrade

--- src/OpenSage.Game/Logic/Object/Upgrade/MaxHealthUpgrade.cs
+++ src/OpenSage.Game/Logic/Object/Upgrade/MaxHealthUpgrade.cs
@@ -1,7 +1,41 @@
 ﻿using OpenSage.Data.Ini;
+using OpenSage.Mathematics.FixedMath;
 
 namespace OpenSage.Logic.Object
 {
+    public sealed class MaxHealthUpgrade : UpgradeModule
+    {
+        private readonly GameObject _gameObject;
+
+        internal MaxHealthUpgrade(GameObject gameObject, MaxHealthUpgradeModuleData moduleData) : base(moduleData)
+        {
+            _gameObject = gameObject;
+        }
+
+        internal override void OnTrigger(BehaviorUpdateContext context, bool triggered)
+        {
+            if (triggered)
+            {
+                var maxHealthUpgrade = _moduleData as MaxHealthUpgradeModuleData;
+                _gameObject.Body.MaxHealth += (Fix64) maxHealthUpgrade.AddMaxHealth;
+
+                switch (maxHealthUpgrade.ChangeType)
+                {
+                    case MaxHealthChangeType.PreserveRatio:
+                        Fix64 ratio = _gameObject.Body.Health / _gameObject.Body.MaxHealth;
+                        _gameObject.Body.Health += ratio * (Fix64) maxHealthUpgrade.AddMaxHealth);
+                        break;
+                    case MaxHealthChangeType.AddCurrentHealthToo:
+                        _gameObject.Body.Health += (Fix64) maxHealthUpgrade.AddMaxHealth;
+                        break;
+                    case MaxHealthChangeType.SameCurrentHealth:
+                        // Don't add any new health
+                        break;
+                }
+            }
+        }
+    }
+
     public sealed class MaxHealthUpgradeModuleData : UpgradeModuleData
     {
         internal static MaxHealthUpgradeModuleData Parse(IniParser parser) => parser.ParseBlock(FieldParseTable);
@@ -15,6 +49,11 @@ namespace OpenSage.Logic.Object
 
         public float AddMaxHealth { get; private set; }
         public MaxHealthChangeType ChangeType { get; private set; }
+
+        internal override BehaviorModule CreateModule(GameObject gameObject)
+        {
+            return new MaxHealthUpgrade(gameObject, this);
+        }
     }
 
     public enum MaxHealthChangeType
@@ -23,6 +62,9 @@ namespace OpenSage.Logic.Object
         PreserveRatio,
 
         [IniEnum("ADD_CURRENT_HEALTH_TOO")]
-        AddCurrentHealthToo
+        AddCurrentHealthToo,
+
+        [IniEnum("SAME_CURRENTHEALTH")]
+        SameCurrentHealth
     }
 }
