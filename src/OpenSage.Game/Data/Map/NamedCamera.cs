05f22caf263c62dbff65d09cc0ad772eb925444b
Stephan Vedder <stephan.vedder@gmail.com>
2020-07-25 00:30:16 +0200

    Cleanup

--- src/OpenSage.Game/Data/Map/NamedCamera.cs
+++ src/OpenSage.Game/Data/Map/NamedCamera.cs
@@ -8,24 +8,19 @@ namespace OpenSage.Data.Map
 {
     public sealed class NamedCamera
     {
-        public Vector2 LookAtPoint { get; private set; }
+        public Vector3 LookAtPoint { get; private set; }
         public string Name { get; private set; }
 
-        public float Pitch { get; private set; }
-        public float Roll { get; private set; }
-        public float Yaw { get; private set; }
+        public Vector3 EulerAngles { get; private set; }
         public float Zoom { get; private set; }
 
         internal static NamedCamera Parse(BinaryReader reader)
         {
-            var lookAt = reader.ReadVector2();
-            var unknown1 = reader.ReadSingle();
+            var lookAt = reader.ReadVector3();
             var name = reader.ReadUInt16PrefixedAsciiString();
             //var rotmat = reader.ReadMatrix3x2();
 
-            var pitch = MathUtility.ToDegrees(reader.ReadSingle());
-            var roll = MathUtility.ToDegrees(reader.ReadSingle());
-            var yaw = MathUtility.ToDegrees(reader.ReadSingle());
+            var eulerAngles = reader.ReadVector3();
 
             var zoom = reader.ReadSingle();
             var unknown2 = reader.ReadSingle();
@@ -35,9 +30,7 @@ namespace OpenSage.Data.Map
             {
                 LookAtPoint = lookAt,
                 Name = name,
-                Pitch = pitch,
-                Roll = roll,
-                Yaw = yaw,
+                EulerAngles = eulerAngles,
                 Zoom = zoom
             };
         }
@@ -45,12 +38,8 @@ namespace OpenSage.Data.Map
         internal void WriteTo(BinaryWriter writer)
         {
             writer.Write(LookAtPoint);
-            writer.Write(0.0f);
             writer.WriteUInt16PrefixedAsciiString(Name);
-
-            writer.Write(MathUtility.ToRadians(Pitch));
-            writer.Write(MathUtility.ToRadians(Roll));
-            writer.Write(MathUtility.ToRadians(Yaw)); ;
+            writer.Write(EulerAngles);
             writer.Write(Zoom);
             writer.Write(Vector2.Zero);
         }
