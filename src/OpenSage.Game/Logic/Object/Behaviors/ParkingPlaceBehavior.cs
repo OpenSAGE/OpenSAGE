f7f42a1f45a7239dfaf7e67fbabeb4a5377af28e
Stephan Vedder <stephan.vedder@gmail.com>
2020-07-25 05:23:35 +0200

    Hacky parking place behavior

--- src/OpenSage.Game/Logic/Object/Behaviors/ParkingPlaceBehavior.cs
+++ src/OpenSage.Game/Logic/Object/Behaviors/ParkingPlaceBehavior.cs
@@ -1,7 +1,51 @@
-﻿using OpenSage.Data.Ini;
+﻿using System;
+using System.Numerics;
+using OpenSage.Data.Ini;
 
 namespace OpenSage.Logic.Object
 {
+    //TODO: HasRunways. Figure out cols & rows
+    public sealed class ParkingPlaceBehaviour : BehaviorModule, IProductionExit
+    {
+        private readonly ParkingPlaceBehaviorModuleData _moduleData;
+        private readonly GameObject _gameObject;
+        private readonly GameContext _gameContext;
+
+        internal ParkingPlaceBehaviour(ParkingPlaceBehaviorModuleData moduleData, GameObject gameObject, GameContext context)
+        {
+            _moduleData = moduleData;
+            _gameObject = gameObject;
+            _gameContext = context;
+        }
+
+        public Vector3 GetUnitCreatePoint()
+        {
+            int runway = _gameContext.Random.Next(1, _moduleData.NumCols);
+            int hangar = _gameContext.Random.Next(1, _moduleData.NumRows);
+            var (modelInstance, bone) = _gameObject.FindBone($"RUNWAY{runway}PARK{hangar}HAN");
+
+            if (bone == null)
+            {
+                throw new InvalidOperationException("Could not find spawn point bone");
+            }
+
+            return bone.Transform.Translation;
+        }
+
+        public Vector3? GetNaturalRallyPoint()
+        {
+            int start = _gameContext.Random.Next(1, _moduleData.NumCols);
+            var (modelInstance, bone) = _gameObject.FindBone($"RUNWAYSTART{start}");
+
+            if (bone == null)
+            {
+                throw new InvalidOperationException("Could not find start point bone");
+            }
+
+            return bone.Transform.Translation;
+        }
+    }
+
     /// <summary>
     /// Used by FS_AIRFIELD KindOfs. If <see cref="HasRunways"/> is set then the model requires 
     /// RunwayStartN, RunwayEndN, RunwayNPrepN, RunwayNParkingN and RunwayNParkNHan bones where N 
@@ -27,5 +71,10 @@ namespace OpenSage.Logic.Object
         public bool HasRunways { get; private set; }
         public int ApproachHeight { get; private set; }
         public bool ParkInHangars { get; private set; }
+
+        internal override BehaviorModule CreateModule(GameObject gameObject, GameContext context)
+        {
+            return new ParkingPlaceBehaviour(this, gameObject, context);
+        }
     }
 }
