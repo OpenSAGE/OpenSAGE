fd5d71fd1da8048f99a2205f8d97ed89cf32064a
Stephan Vedder <stephan.vedder@gmail.com>
2020-07-28 22:14:16 +0200

    Implement simple tree draw module

--- src/OpenSage.Game/Logic/Object/Draw/W3dTreeDraw.cs
+++ src/OpenSage.Game/Logic/Object/Draw/W3dTreeDraw.cs
@@ -1,20 +1,83 @@
-﻿using OpenSage.Data.Ini;
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using OpenSage.Content;
+using OpenSage.Data.Ini;
+using OpenSage.FX;
+using OpenSage.Graphics;
+using OpenSage.Graphics.Cameras;
+using OpenSage.Graphics.Rendering;
+using OpenSage.Graphics.Shaders;
+using OpenSage.Mathematics;
+using Veldrid;
 
 namespace OpenSage.Logic.Object
 {
+    [AddedIn(SageGame.Bfme)]
+    public sealed class W3dTreeDraw : DrawModule
+    {
+        private readonly GameContext _gameContext;
+        private ModelInstance _modelInstance;
+        private readonly W3dTreeDrawModuleData _moduleData;
+
+        public override IEnumerable<BitArray<ModelConditionFlag>> ModelConditionStates { get; } = Array.Empty<BitArray<ModelConditionFlag>>();
+
+        internal W3dTreeDraw(W3dTreeDrawModuleData moduleData, GameContext context)
+        {
+            _gameContext = context;
+            _moduleData = moduleData;
+            _modelInstance = AddDisposable(_moduleData.Model.Value.CreateInstance(_gameContext.AssetLoadContext));
+            //TODO: overwrite texture somehow & take care of other fields
+        }
+
+        internal override void BuildRenderList(RenderList renderList, Camera camera, bool castsShadow, MeshShaderResources.RenderItemConstantsPS renderItemConstantsPS)
+        {
+            _modelInstance.BuildRenderList(
+                renderList,
+                camera,
+                castsShadow,
+                renderItemConstantsPS);
+        }
+
+        internal override (ModelInstance, ModelBone) FindBone(string boneName)
+        {
+            throw new NotSupportedException();
+        }
+
+        internal override string GetWeaponFireFXBone(WeaponSlot slot)
+        {
+            throw new NotSupportedException();
+        }
+
+        internal override string GetWeaponLaunchBone(WeaponSlot slot)
+        {
+            throw new NotSupportedException();
+        }
+
+        internal override void SetWorldMatrix(in Matrix4x4 worldMatrix)
+        {
+            _modelInstance.SetWorldMatrix(worldMatrix);
+        }
+
+        internal override void Update(in TimeInterval time)
+        {
+            _modelInstance.Update(time);
+        }
+    }
+
     public sealed class W3dTreeDrawModuleData : DrawModuleData
     {
         internal static W3dTreeDrawModuleData Parse(IniParser parser) => parser.ParseBlock(FieldParseTable);
 
         private static readonly IniParseTable<W3dTreeDrawModuleData> FieldParseTable = new IniParseTable<W3dTreeDrawModuleData>
         {
-            { "ModelName", (parser, x) => x.ModelName = parser.ParseFileName() },
-            { "TextureName", (parser, x) => x.TextureName = parser.ParseFileName() },
+            { "ModelName", (parser, x) => x.Model = parser.ParseModelReference() },
+            { "TextureName", (parser, x) => x.Texture = parser.ParseTextureReference() },
 
             { "DoTopple", (parser, x) => x.DoTopple = parser.ParseBoolean() },
             { "DoShadow", (parser, x) => x.DoShadow = parser.ParseBoolean() },
-            { "ToppleFX", (parser, x) => x.ToppleFX = parser.ParseAssetReference() },
-            { "BounceFX", (parser, x) => x.BounceFX = parser.ParseAssetReference() },
+            { "ToppleFX", (parser, x) => x.ToppleFX = parser.ParseFXListReference() },
+            { "BounceFX", (parser, x) => x.BounceFX = parser.ParseFXListReference() },
             { "KillWhenFinishedToppling", (parser, x) => x.KillWhenFinishedToppling = parser.ParseBoolean() },
             { "SinkDistance", (parser, x) => x.SinkDistance = parser.ParseInteger() },
             { "SinkTime", (parser, x) => x.SinkTime = parser.ParseInteger() },
@@ -27,17 +90,17 @@ namespace OpenSage.Logic.Object
             { "TaintedTree", (parser, x) => x.TaintedTree = parser.ParseBoolean() },
             { "FadeDistance", (parser, x) => x.FadeDistance = parser.ParseInteger() },
             { "FadeTarget", (parser, x) => x.FadeTarget = parser.ParseInteger() },
-            { "MorphFX", (parser, x) => x.MorphFX = parser.ParseAssetReference() },
+            { "MorphFX", (parser, x) => x.MorphFX = parser.ParseFXListReference() },
             { "MorphTree", (parser, x) => x.MorphTree = parser.ParseIdentifier() }
         };
 
-        public string ModelName { get; private set; }
-        public string TextureName { get; private set; }
+        public LazyAssetReference<Model> Model { get; private set; }
+        public LazyAssetReference<TextureAsset> Texture { get; private set; }
 
         public bool DoTopple { get; private set; }
         public bool DoShadow { get; private set; }
-        public string ToppleFX { get; private set; }
-        public string BounceFX { get; private set; }
+        public LazyAssetReference<FXList> ToppleFX { get; private set; }
+        public LazyAssetReference<FXList> BounceFX { get; private set; }
         public bool KillWhenFinishedToppling { get; private set; }
         public int SinkDistance { get; private set; }
         public int SinkTime { get; private set; }
@@ -67,9 +130,14 @@ namespace OpenSage.Logic.Object
         public int FadeTarget { get; private set; }
 
         [AddedIn(SageGame.Bfme)]
-        public string MorphFX { get; private set; }
+        public LazyAssetReference<FXList> MorphFX { get; private set; }
 
         [AddedIn(SageGame.Bfme)]
         public string MorphTree { get; private set; }
+
+        internal override DrawModule CreateDrawModule(GameObject gameObject, GameContext context)
+        {
+            return new W3dTreeDraw(this, context);
+        }
     }
 }
