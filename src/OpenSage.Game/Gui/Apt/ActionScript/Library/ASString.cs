7f653015b55a69a6d5d5112edb2fd03ee492887c
Stephan Vedder <stephan.vedder@gmail.com>
2020-08-11 00:07:44 +0200

    Implement common class for builtins

--- src/OpenSage.Game/Gui/Apt/ActionScript/Library/ASString.cs
+++ src/OpenSage.Game/Gui/Apt/ActionScript/Library/ASString.cs
@@ -1,59 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace OpenSage.Gui.Apt.ActionScript.Library
 {
-    class ASString : ObjectContext
+    class ASString : BuiltinClass
     {
-        private readonly Dictionary<string, Func<Value[], Value>> _builtinFunctions;
-        private readonly Dictionary<string, Func<Value>> _builtinVariablesGet;
-
         string _value;
 
-        public ASString(string value)
+        public ASString(string value) : base()
         {
             _value = value;
 
             //list of builtin functions
-            _builtinFunctions = new Dictionary<string, Func<Value[], Value>>
-            {
-                ["substr"] = (Value[] args) => args.Length == 1 ? Value.FromString(_value.Substring(args[0].ToInteger()))
-                                                                : Value.FromString(_value.Substring(args[0].ToInteger(), args[1].ToInteger()))
-            };
+            _builtinFunctions.Add("substr", substr);
 
             // list of builtin variables
-            _builtinVariablesGet = new Dictionary<string, Func<Value>>
+            _builtinVariablesGet.Add("length", () => Value.FromInteger(_value.Length));
+        }
+
+        private Value substr(Value[] args)
+        {
+            if (args.Length == 1)
             {
-                ["length"] = () => Value.FromInteger(_value.Length)
-            };
-        }
-
-        public override bool IsBuiltInVariable(string name)
-        {
-            return _builtinVariablesGet.ContainsKey(name);
-        }
-
-        public override Value GetBuiltInVariable(string name)
-        {
-            return _builtinVariablesGet[name]();
-        }
-
-        public override bool IsBuiltInFunction(string name)
-        {
-            if (_builtinFunctions.ContainsKey(name))
-            {
-                return true;
+                return Value.FromString(_value.Substring(args[0].ToInteger()));
             }
-
-            return false;
-        }
-
-        public override void CallBuiltInFunction(ActionContext context, string name, Value[] args)
-        {
-            Value result = _builtinFunctions[name](args);
-            if (result.Type != ValueType.Undefined)
+            else if (args.Length == 2)
             {
-                context.Push(result);
+                return Value.FromString(_value.Substring(args[0].ToInteger(), args[1].ToInteger()));
+            }
+            else
+            {
+                throw new InvalidOperationException("Argument count invalid!");
             }
         }
     }
