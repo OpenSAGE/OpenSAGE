5ede2cf90a4305f914ee6301303252c9a8e2e950
Michael Schnabel <michael-schnabel@gmx.net>
2020-08-14 01:06:25 +0200

    made ready for review

--- src/OpenSage.Game/Logic/Object/Update/AIUpdate/AIUpdate.cs
+++ src/OpenSage.Game/Logic/Object/Update/AIUpdate/AIUpdate.cs
@@ -122,8 +122,8 @@ namespace OpenSage.Logic.Object
             if (_currentLocomotor != null && TargetPoints.Count > 0)
             {
                 Vector3? nextPoint = null;
-                if (TargetPoints.Count > 1) nextPoint = TargetPoints[1];
 
+                if (TargetPoints.Count > 1) nextPoint = TargetPoints[1];
 
                 var reachedPosition = _currentLocomotor.MoveTowardsPosition(context.Time, TargetPoints[0], context.GameContext.Terrain.HeightMap, nextPoint);
 
