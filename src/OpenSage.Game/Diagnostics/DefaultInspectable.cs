4ecfbc3e5a8f3e5a2613ce33702a26b658287921
Tim Jones <tim@timjones.io>
2020-08-31 21:38:58 +0100

    Add Preview window

--- src/OpenSage.Game/Diagnostics/DefaultInspectable.cs
+++ src/OpenSage.Game/Diagnostics/DefaultInspectable.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Reflection;
 using ImGuiNET;
+using OpenSage.Content;
 using OpenSage.Diagnostics.Util;
 using OpenSage.Mathematics;
 
@@ -11,6 +13,190 @@ namespace OpenSage.Diagnostics
 {
     internal sealed class DefaultInspectable : IInspectable
     {
+        private delegate bool InspectableDrawerDrawDelegate(ref object propertyValue, DiagnosticViewContext context);
+
+        private sealed class InspectableDrawer
+        {
+            public readonly Type Type;
+            public readonly InspectableDrawerDrawDelegate Draw;
+            public readonly bool HasChildNodes;
+
+            public InspectableDrawer(Type type, InspectableDrawerDrawDelegate draw, bool hasChildNodes = false)
+            {
+                Type = type;
+                Draw = draw;
+                HasChildNodes = hasChildNodes;
+            }
+        }
+
+        private static readonly List<InspectableDrawer> Drawers;
+
+        static DefaultInspectable()
+        {
+            Drawers = new List<InspectableDrawer>();
+
+            Drawers.Add(new InspectableDrawer(typeof(string), (ref object v, DiagnosticViewContext context) =>
+            {
+                var s = (string) v;
+                if (ImGui.InputText("", ref s, 1000))
+                {
+                    v = s;
+                    return true;
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(bool), (ref object v, DiagnosticViewContext context) =>
+            {
+                var b = (bool) v;
+                if (ImGui.Checkbox("", ref b))
+                {
+                    v = b;
+                    return true;
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(int), (ref object v, DiagnosticViewContext context) =>
+            {
+                var i = (int) v;
+                if (ImGui.DragInt("", ref i))
+                {
+                    v = i;
+                    return true;
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(float), (ref object v, DiagnosticViewContext context) =>
+            {
+                var f = (float) v;
+                if (ImGui.DragFloat("", ref f))
+                {
+                    v = f;
+                    return true;
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(Vector3), (ref object v, DiagnosticViewContext context) =>
+            {
+                var c = (Vector3) v;
+                if (ImGui.DragFloat3("", ref c))
+                {
+                    v = c;
+                    return true;
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(Percentage), (ref object v, DiagnosticViewContext context) =>
+            {
+                var f = (float) (Percentage) v;
+                if (ImGui.DragFloat("", ref f))
+                {
+                    v = new Percentage(f);
+                    return true;
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(ColorRgb), (ref object v, DiagnosticViewContext context) =>
+            {
+                var c = ((ColorRgb) v).ToVector3();
+                if (ImGui.ColorEdit3("", ref c))
+                {
+                    v = new ColorRgb(
+                        (byte) (c.X * 255.0f),
+                        (byte) (c.Y * 255.0f),
+                        (byte) (c.Z * 255.0f));
+                    return true;
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(ColorRgba), (ref object v, DiagnosticViewContext context) =>
+            {
+                var c = ((ColorRgba) v).ToVector4();
+                if (ImGui.ColorEdit4("", ref c))
+                {
+                    v = new ColorRgba(
+                        (byte) (c.X * 255.0f),
+                        (byte) (c.Y * 255.0f),
+                        (byte) (c.Z * 255.0f),
+                        (byte) (c.W * 255.0f));
+                    return true;
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(ColorRgbF), (ref object v, DiagnosticViewContext context) =>
+            {
+                var c = ((ColorRgbF) v).ToVector3();
+                if (ImGui.ColorEdit3("", ref c))
+                {
+                    v = new ColorRgbF(c.X, c.Y, c.Z);
+                    return true;
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(ColorRgbaF), (ref object v, DiagnosticViewContext context) =>
+            {
+                var c = ((ColorRgbaF) v).ToVector4();
+                if (ImGui.ColorEdit4("", ref c))
+                {
+                    v = new ColorRgbaF(c.X, c.Y, c.Z, c.W);
+                    return true;
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(Enum), (ref object v, DiagnosticViewContext context) =>
+            {
+                var e = (Enum) v;
+                if (ImGuiUtility.ComboEnum(v.GetType(), "", ref e))
+                {
+                    v = e;
+                    return true;
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(ILazyAssetReference), (ref object v, DiagnosticViewContext context) =>
+            {
+                var asset = (ILazyAssetReference) v;
+                if (asset.Value != null)
+                {
+                    if (ImGui.Button(asset.Value.FullName))
+                    {
+                        context.SelectedObject = asset.Value;
+                    }
+                }
+                else
+                {
+                    ImGui.Text("<null>");
+                }
+                return false;
+            }));
+
+            Drawers.Add(new InspectableDrawer(typeof(BaseAsset), (ref object v, DiagnosticViewContext context) =>
+            {
+                var asset = (BaseAsset) v;
+                if (ImGui.Button(asset.FullName))
+                {
+                    context.SelectedObject = v;
+                }
+                return false;
+            }));
+
+            // Order matters here - this must be last.
+            Drawers.Add(new InspectableDrawer(typeof(object), (ref object v, DiagnosticViewContext context) =>
+            {
+                return false;
+            }, hasChildNodes: true));
+        }
+
         private readonly object _value;
         private readonly DiagnosticViewContext _context;
 
@@ -24,7 +210,11 @@ namespace OpenSage.Diagnostics
 
         void IInspectable.DrawInspector()
         {
+            ImGuiUtility.BeginPropertyList();
+
             DrawObject(_value);
+
+            ImGuiUtility.EndPropertyList();
         }
 
         private void DrawObject(object instance)
@@ -34,8 +224,12 @@ namespace OpenSage.Diagnostics
                 var i = 0;
                 foreach (var item in (IEnumerable) instance)
                 {
+                    ImGui.PushID(i);
+
                     DrawRow($"Element {i} ({item.GetType().Name})", item, null);
                     i++;
+
+                    ImGui.PopID();
                 }
             }
             else
@@ -49,6 +243,7 @@ namespace OpenSage.Diagnostics
                     .OrderBy(x => x.Name)
                     .ToArray();
 
+                var i = 0;
                 foreach (var memberInfo in memberInfos)
                 {
                     var value = memberInfo switch
@@ -73,126 +268,61 @@ namespace OpenSage.Diagnostics
                                 throw new InvalidOperationException();
                         }
                     }
+
+                    ImGui.PushID(i);
+
                     DrawRow(memberInfo.Name, value, SetValue);
+                    i++;
+
+                    ImGui.PopID();
                 }
             }
         }
 
         private void DrawRow(string name, object propertyValue, Action<object> setPropertyValue)
         {
+            InspectableDrawer drawer = null;
+            var hasChildNodes = false;
+            if (propertyValue != null)
+            {
+                var propertyType = propertyValue.GetType();
+                drawer = Drawers.First(x => x.Type.IsAssignableFrom(propertyType));
+                hasChildNodes = drawer.HasChildNodes;
+            }
+
             ImGui.AlignTextToFramePadding();
 
-            switch (propertyValue)
+            var nodeOpen = false;
+            if (hasChildNodes)
             {
-                case string s:
-                    if (ImGui.InputText(name, ref s, 1000))
-                    {
-                        setPropertyValue?.Invoke(s);
-                    }
-                    break;
+                nodeOpen = ImGui.TreeNode(name);
+            }
+            else
+            {
+                ImGui.TreeNodeEx(name, ImGuiTreeNodeFlags.Leaf | ImGuiTreeNodeFlags.NoTreePushOnOpen);
+            }
 
-                case bool b:
-                    if (ImGui.Checkbox(name, ref b))
-                    {
-                        setPropertyValue?.Invoke(b);
-                    }
-                    break;
+            ImGui.NextColumn();
+            ImGui.AlignTextToFramePadding();
 
-                case int i:
-                    if (ImGui.DragInt(name, ref i))
-                    {
-                        setPropertyValue?.Invoke(i);
-                    }
-                    break;
+            if (drawer != null)
+            {
+                if (drawer.Draw(ref propertyValue, _context))
+                {
+                    setPropertyValue?.Invoke(propertyValue);
+                }
+            }
+            else
+            {
+                ImGui.Text("<null>");
+            }
 
-                case float f:
-                    if (ImGui.DragFloat(name, ref f))
-                    {
-                        setPropertyValue?.Invoke(f);
-                    }
-                    break;
+            ImGui.NextColumn();
 
-                case Percentage p:
-                    var percentage = (float) p;
-                    if (ImGui.DragFloat(name, ref percentage))
-                    {
-                        setPropertyValue?.Invoke(new Percentage(percentage));
-                    }
-                    break;
-
-                case ColorRgb rgb:
-                    var colorRgb = rgb.ToVector3();
-                    if (ImGui.ColorEdit3(name, ref colorRgb))
-                    {
-                        setPropertyValue?.Invoke(new ColorRgb(
-                            (byte) (colorRgb.X * 255.0f),
-                            (byte) (colorRgb.Y * 255.0f),
-                            (byte) (colorRgb.Z * 255.0f)));
-                    }
-                    break;
-
-                case ColorRgba rgba:
-                    var colorRgba = rgba.ToVector4();
-                    if (ImGui.ColorEdit4(name, ref colorRgba))
-                    {
-                        setPropertyValue?.Invoke(new ColorRgba(
-                            (byte) (colorRgba.X * 255.0f),
-                            (byte) (colorRgba.Y * 255.0f),
-                            (byte) (colorRgba.Z * 255.0f),
-                            (byte) (colorRgba.W * 255.0f)));
-                    }
-                    break;
-
-                case ColorRgbaF rgbaf:
-                    var colorRgbaF = rgbaf.ToVector4();
-                    if (ImGui.ColorEdit4(name, ref colorRgbaF))
-                    {
-                        setPropertyValue?.Invoke(new ColorRgbaF(
-                            colorRgbaF.X,
-                            colorRgbaF.Y,
-                            colorRgbaF.Z,
-                            colorRgbaF.W));
-                    }
-                    break;
-
-                case Vector3 v3:
-                    if (ImGui.DragFloat3(name, ref v3))
-                    {
-                        setPropertyValue?.Invoke(v3);
-                    }
-                    break;
-
-                case Enum e:
-                    if (ImGuiUtility.ComboEnum(propertyValue.GetType(), name, ref e))
-                    {
-                        setPropertyValue?.Invoke(e);
-                    }
-                    break;
-
-                case BaseAsset asset:
-                    // TODO: Show combo to allow asset to be changed.
-                    if (ImGui.Selectable(name))
-                    {
-                        _context.SelectedObject = new DefaultInspectable(asset, _context);
-                    }
-                    break;
-
-                default:
-                    var hasChildNodes = propertyValue != null &&
-                        Type.GetTypeCode(propertyValue.GetType()) == TypeCode.Object;
-                    if (hasChildNodes)
-                    {
-                        if (ImGui.TreeNode(name))
-                        {
-                            DrawObject(propertyValue);
-                            ImGui.TreePop();
-                        }
-                    }
-                    else
-                    {
-                        ImGui.LabelText(name, propertyValue?.ToString() ?? "<null>");
-                    }
-                    break;
+            if (nodeOpen)
+            {
+                DrawObject(propertyValue);
+                ImGui.TreePop();
             }
         }
     }
