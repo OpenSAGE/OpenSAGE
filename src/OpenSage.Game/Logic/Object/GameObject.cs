78b8a8c41e80e76592e2f18a24dacf94bcd30d56
Michael Schnabel <michael-schnabel@gmx.net>
2020-10-02 22:07:35 +0200

    unpack correct castle, limit amount of each hero to 1

--- src/OpenSage.Game/Logic/Object/GameObject.cs
+++ src/OpenSage.Game/Logic/Object/GameObject.cs
@@ -639,6 +639,19 @@ namespace OpenSage.Logic.Object
             return true;
         }
 
+        public bool CanRecruitHero(ObjectDefinition definition)
+        {
+            foreach (var obj in Parent.Items)
+            {
+                if (obj.Definition.Name == definition.Name
+                    && obj.Owner == Owner)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         public bool CanProduceObject(ObjectDefinition definition)
         {
             return ProductionUpdate?.CanProduceObject(definition) ?? true;
