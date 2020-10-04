f302df291c669c6de98bd470b4cddcb86c391396
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2020-10-04 15:42:25 +0200

    Implemented script actions and conditions necessary for music playback

---
+++ src/OpenSage.Game/Scripting/Conditions/ScriptConditions.Multimedia.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenSage.Scripting
+{
+    partial class ScriptConditions
+    {
+        [ScriptCondition(ScriptConditionType.MusicTrackHasCompleted, "Multimedia/Finished/Music track has completed some number of times", "'{0}' has completed at least {1} times. (NOTE: This can only be used to start other music. USING THIS SCRIPT IN ANY OTHER WAY WILL CAUSE REPLAYS TO NOT WORK.)")]
+        public static bool MusicTrackHasCompleted(ScriptExecutionContext context, [ScriptArgumentType(ScriptArgumentType.MusicName)] string trackName, [ScriptArgumentType(ScriptArgumentType.Integer)] int count)
+        {
+            return context.Scene.Audio.GetFinishedCount(trackName) >= count;
+        }
+    }
+}
