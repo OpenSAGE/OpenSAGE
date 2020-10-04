213233c1be7baf36b01dad4470b9ae1935368472
Michael Schnabel <michael-schnabel@gmx.net>
2020-10-04 14:17:26 +0200

    some more work on bfme palantir buttons

--- src/OpenSage.Game/Data/Apt/ConstantData.cs
+++ src/OpenSage.Game/Data/Apt/ConstantData.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using OpenSage.Data.Utilities.Extensions;
 using OpenSage.FileFormats;
 
 namespace OpenSage.Data.Apt
@@ -18,70 +16,75 @@ namespace OpenSage.Data.Apt
 
         public static ConstantData FromFileSystemEntry(FileSystemEntry entry)
         {
-            using (var stream = entry.Open())
-            using (var reader = new BinaryReader(stream))
+            using var stream = entry.Open();
+            using var reader = new BinaryReader(stream);
+            var data = new ConstantData();
+
+            //validate that this is a correct const
+            var magic = reader.ReadFixedLengthString(17);
+            if (magic != "Apt constant file")
             {
-                var data = new ConstantData();
+                throw new InvalidDataException($"Not a supported const file: {magic}");
+            }
 
-                //validate that this is a correct const              
-                var magic = reader.ReadFixedLengthString(17);
-                if (magic != "Apt constant file")
-                    throw new InvalidDataException($"Not a supported const file: {magic}");
+            reader.BaseStream.Seek(3, SeekOrigin.Current);
 
-                reader.BaseStream.Seek(3, SeekOrigin.Current);
+            data.AptDataEntryOffset = reader.ReadUInt32();
+            var numEntries = reader.ReadUInt32();
+            var headerSize = reader.ReadUInt32();
 
-                data.AptDataEntryOffset = reader.ReadUInt32();
-                var numEntries = reader.ReadUInt32();
-                var headerSize = reader.ReadUInt32();
+            if (headerSize != 32)
+            {
+                throw new InvalidDataException("Constant header must be 32 bytes");
+            }
 
-                if (headerSize != 32)
-                    throw new InvalidDataException("Constant header must be 32 bytes");
-
-                for (var i = 0; i < numEntries; i++)
+            for (var i = 0; i < numEntries; i++)
+            {
+                var constEntry = new ConstantEntry
                 {
-                    var constEntry = new ConstantEntry
-                    {
-                        Type = reader.ReadUInt32AsEnum<ConstantEntryType>()
-                    };
+                    Type = reader.ReadUInt32AsEnum<ConstantEntryType>()
+                };
 
-                    //read the number/ string offset
+                //read the number/ string offset
 
 
-                    switch (constEntry.Type)
-                    {
-                        case ConstantEntryType.Undef:
-                            throw new InvalidDataException("Undefined const entry");
-                        case ConstantEntryType.String:
-                            var strOffset = reader.ReadUInt32();
-                            var pos = reader.BaseStream.Position;
-                            reader.BaseStream.Seek(strOffset, SeekOrigin.Begin);
-                            constEntry.Value = reader.ReadNullTerminatedString();
-                            reader.BaseStream.Seek(pos, SeekOrigin.Begin);
-                            break;
-                        case ConstantEntryType.Register:
-                            constEntry.Value = reader.ReadUInt32();
-                            break;
-                        case ConstantEntryType.Boolean:
-                            constEntry.Value = reader.ReadBooleanUInt32Checked();
-                            break;
-                        case ConstantEntryType.Float:
-                            constEntry.Value = reader.ReadSingle();
-                            break;
-                        case ConstantEntryType.Integer:
-                            constEntry.Value = reader.ReadInt32();
-                            break;
-                        case ConstantEntryType.Lookup:
-                            constEntry.Value = reader.ReadUInt32();
-                            break;
-                        default:
-                            throw new InvalidDataException();
-                    }
-
-                    data.Entries.Add(constEntry);
+                switch (constEntry.Type)
+                {
+                    case ConstantEntryType.Undef:
+                        throw new InvalidDataException("Undefined const entry");
+                    case ConstantEntryType.String:
+                        var strOffset = reader.ReadUInt32();
+                        var pos = reader.BaseStream.Position;
+                        reader.BaseStream.Seek(strOffset, SeekOrigin.Begin);
+                        constEntry.Value = reader.ReadNullTerminatedString();
+                        reader.BaseStream.Seek(pos, SeekOrigin.Begin);
+                        break;
+                    case ConstantEntryType.Register:
+                        constEntry.Value = reader.ReadUInt32();
+                        break;
+                    case ConstantEntryType.Boolean:
+                        constEntry.Value = reader.ReadBooleanUInt32Checked();
+                        break;
+                    case ConstantEntryType.Float:
+                        constEntry.Value = reader.ReadSingle();
+                        break;
+                    case ConstantEntryType.Integer:
+                        constEntry.Value = reader.ReadInt32();
+                        break;
+                    case ConstantEntryType.Lookup:
+                        constEntry.Value = reader.ReadUInt32();
+                        break;
+                    case ConstantEntryType.None:
+                        constEntry.Value = null;
+                        break;
+                    default:
+                        throw new InvalidDataException();
                 }
 
-                return data;
+                data.Entries.Add(constEntry);
             }
+
+            return data;
         }
     }
 
--- src/OpenSage.Mods.Bfme/Gui/AptMainMenu.cs
+++ src/OpenSage.Mods.Bfme/Gui/AptMainMenu.cs
@@ -1,23 +1,24 @@
-﻿using OpenSage.Gui.Apt;
+﻿using OpenSage.Data.Apt;
+using OpenSage.Gui.Apt;
 using OpenSage.Gui.Apt.ActionScript;
+using Veldrid;
 
 namespace OpenSage.Mods.Bfme.Gui
 {
-    [AptCallbacks(SageGame.Bfme)]
+    [AptCallbacks(SageGame.Bfme, SageGame.Bfme2, SageGame.Bfme2Rotwk)]
     static class AptMainMenu
     {
         // Called after the initialization has been performed
         public static void OnInitialized(string param, ActionContext context, AptWindow window, Game game)
         {
-            // Set the logo texture
+            // Set a custom render callback
+            var logoTexture = game.GetMappedImage("LogoWithShadow").Texture.Value;
             var imageSprite = window.Root.ScriptObject.Variables["Image"].ToObject().Item as SpriteItem;
             var shape = imageSprite.Content.Items[1] as RenderItem;
-            shape.Texture = game.GetMappedImage("LogoWithShadow").Texture.Value;
-        }
-
-        public static void StopGameMovie(string param, ActionContext context, AptWindow window, Game game)
-        {
-            //TODO: somehow pause the playback
+            shape.RenderCallback = (AptRenderingContext renderContext, Geometry geom, Texture orig) =>
+            {
+                renderContext.RenderGeometry(geom, logoTexture);
+            };
         }
 
         // Close the game
@@ -26,19 +27,68 @@ namespace OpenSage.Mods.Bfme.Gui
             game.Window.Close();
         }
 
-        public static void MultiplayerButtonPressed(string param, ActionContext context, AptWindow window, Game game)
+        public static void StopGameMovie(string param, ActionContext context, AptWindow window, Game game)
         {
-            //TODO: do something
-        }
 
-        public static void BattleSchool(string param, ActionContext context, AptWindow window, Game game)
-        {
-            //TODO: do something
         }
 
         public static void ResetResolution(string param, ActionContext context, AptWindow window, Game game)
         {
             // Probably used for debug purposes
+        }
+
+        public static void CreateAHero(string param, ActionContext context, AptWindow window, Game game)
+        {
+            var aptWindow = game.LoadAptWindow("CreateAHero.apt");
+
+            game.Scene2D.AptWindowManager.QueryTransition(aptWindow);
+        }
+
+        public static void Options(string param, ActionContext context, AptWindow window, Game game)
+        {
+            var aptWindow = game.LoadAptWindow("Options.apt");
+
+            game.Scene2D.AptWindowManager.QueryTransition(aptWindow);
+        }
+
+        public static void Skirmish(string param, ActionContext context, AptWindow window, Game game)
+        {
+            var aptWindow = game.LoadAptWindow("Skirmish.apt");
+
+            game.Scene2D.AptWindowManager.QueryTransition(aptWindow);
+        }
+
+        public static void LAN(string param, ActionContext context, AptWindow window, Game game)
+        {
+            var aptWindow = game.LoadAptWindow("LanOpenPlay.apt");
+
+            game.Scene2D.AptWindowManager.QueryTransition(aptWindow);
+        }
+
+        public static void Credits(string param, ActionContext context, AptWindow window, Game game)
+        {
+            var aptWindow = game.LoadAptWindow("LanOpenPlay.apt");
+
+            game.Scene2D.AptWindowManager.QueryTransition(aptWindow);
+        }
+
+        public static void OnTutorial(string param, ActionContext context, AptWindow window, Game game)
+        {
+            var aptWindow = game.LoadAptWindow("Skirmish.apt");
+
+            game.Scene2D.AptWindowManager.QueryTransition(aptWindow);
+        }
+
+        public static void MultiplayerButtonPressed(string param, ActionContext context, AptWindow window, Game game)
+        {
+            var aptWindow = game.LoadAptWindow("MPGameSetup.apt");
+
+            game.Scene2D.AptWindowManager.QueryTransition(aptWindow);
+        }
+
+        public static void BattleSchool(string param, ActionContext context, AptWindow window, Game game)
+        {
+
         }
     }
 }
