7a68ee493625781ca59e8518c87637d6a9610189
Michael Schnabel <michael-schnabel@gmx.net>
2020-10-17 18:33:44 +0200

    small fixes

--- src/OpenSage.Game/Logic/Object/GameObject.cs
+++ src/OpenSage.Game/Logic/Object/GameObject.cs
@@ -526,7 +526,7 @@ namespace OpenSage.Logic.Object
                 var passed = gameTime.TotalTime - ConstructionStart;
                 BuildProgress = Math.Clamp((float) passed.TotalSeconds / Definition.BuildTime, 0.0f, 1.0f);
 
-                if (MathF.Abs(BuildProgress - 1.0f) < 0.01f)
+                if (BuildProgress >= 1.0f)
                 {
                     FinishConstruction();
                 }
@@ -578,10 +578,10 @@ namespace OpenSage.Logic.Object
 
         internal void StartConstruction(in TimeInterval gameTime)
         {
-            if (Definition.KindOf == null)
-            {
-                return;
-            }
+            //if (Definition.KindOf == null)
+            //{
+            //    return;
+            //}
 
             if (Definition.KindOf.Get(ObjectKinds.Structure))
             {
