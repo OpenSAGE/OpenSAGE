a357de768e66be6f56828879c0c3580e3871dc00
lanyi <lanyi@ra3.moe>
2020-11-16 06:25:38 +0100

    Create geometry and xml files in apt editor export

--- src/OpenSage.Tools.AptEditor/Apt/AptDataDump.cs
+++ src/OpenSage.Tools.AptEditor/Apt/AptDataDump.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 using OpenSage.Data.Apt;
 using OpenSage.Tools.AptEditor.Apt.Writer;
 
@@ -6,32 +8,53 @@ namespace OpenSage.Tools.AptEditor.Apt
 {
     public sealed class AptDataDump
     {
+        public string Name { get; }
         public byte[] AptData { get; }
         public byte[] ConstantData { get; }
         public byte[] ImageMapData { get; }
-        // TODO: public Dictionary<string, byte[]> GeometryData;
+        public Dictionary<uint, string> GeometryData { get; }
+        // TODO: textures
+
         public AptDataDump(AptFile file)
         {
+            Name = file.MovieName;
             var aptData = AptDataWriter.Write(file.Movie);
             AptData = aptData.Data;
             ConstantData = ConstantDataWriter.Write(aptData.EntryOffset, file.Constants);
             ImageMapData = ImageMapWriter.Write(file.ImageMap);
+            GeometryData = file.GeometryMap.ToDictionary(t => t.Key, t => t.Value.RawText);
         }
 
         public void WriteTo(DirectoryInfo target)
         {
             target.Create();
+            // TODO: use a true XML serializer
+            using var xml = new StreamWriter(File.OpenWrite(Path.Combine(target.FullName, $"{Name}.xml")));
+            xml.WriteLine("<?xml version='1.0' encoding='utf-8'?>");
+            xml.WriteLine("<AssetDeclaration xmlns=\"uri:ea.com:eala:asset\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance \">");
             var list = new[]
             {
-                (AptData, ".apt"),
-                (ConstantData, ".const"),
-                (ImageMapData, ".dat")
+                (AptData, "Apt"),
+                (ConstantData, "Const"),
+                (ImageMapData, "Dat")
             };
-            foreach (var (data, extension) in list)
+            foreach (var (data, type) in list)
             {
-                var fileName = Path.Combine(target.FullName, $"{target.Name}{extension}");
-                File.WriteAllBytes(fileName, data);
+                var extension = type.ToLowerInvariant();
+                var fileName = $"{Name}.{extension}";
+                File.WriteAllBytes(Path.Combine(target.FullName, fileName), data);
+                xml.WriteLine($"    <Apt{type}Data id=\"{Name}_{extension}\" File=\"{fileName}\" />");
             }
+
+            var geometryDirectory = target.CreateSubdirectory($"{Name}_geometry");
+            foreach (var (id, data) in GeometryData)
+            {
+                var ruName = Path.Combine(geometryDirectory.Name, $"{id}.ru");
+                xml.WriteLine($"    <AptGeometryData id=\"{Name}_{id}\" File=\"{ruName}\" AptID=\"{id}\"/>");
+                File.WriteAllText(Path.Combine(target.FullName, ruName), data);
+            }
+
+            xml.WriteLine("</AssetDeclaration>");
         }
     }
 
