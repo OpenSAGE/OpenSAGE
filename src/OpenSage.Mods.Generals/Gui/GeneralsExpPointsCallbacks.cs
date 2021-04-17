ba78c58046f1a893c8b738969860077f497fa9c2
Stephan Vedder <stephan.vedder@gmail.com>
2021-04-17 03:34:16 +0200

    implement exp points callbacks

--- src/OpenSage.Mods.Generals/Gui/GeneralsExpPointsCallbacks.cs
+++ src/OpenSage.Mods.Generals/Gui/GeneralsExpPointsCallbacks.cs
@@ -27,8 +27,38 @@ namespace OpenSage.Mods.Generals.Gui
             var rank1 = game.Scene3D.LocalPlayer.Template.PurchaseScienceCommandSetRank1;
             for (int i = 0; i < 3; i++)
             {
-                var buttonControl = window.Controls.FindControl("ButtonRank1Number" + i);
-                if (rank1 != null && rank1.Value.Buttons.TryGetValue(i, out var commandButtonReference))
+                var buttonControl = window.Controls.FindControl("GeneralsExpPoints.wnd:ButtonRank1Number" + i);
+                if (rank1 != null && rank1.Value.Buttons.TryGetValue(i + 1, out var commandButtonReference))
+                {
+                    var commandButton = commandButtonReference.Value;
+
+                    buttonControl.BackgroundImage = window.ImageLoader.CreateFromMappedImageReference(commandButton.ButtonImage);
+
+                    buttonControl.DisabledBackgroundImage = buttonControl.BackgroundImage?.WithGrayscale(true);
+
+                }
+            }
+
+            var rank3 = game.Scene3D.LocalPlayer.Template.PurchaseScienceCommandSetRank3;
+            for (int i = 0; i < 9; i++)
+            {
+                var buttonControl = window.Controls.FindControl("GeneralsExpPoints.wnd:ButtonRank3Number" + i);
+                if (rank1 != null && rank3.Value.Buttons.TryGetValue(i + 1, out var commandButtonReference))
+                {
+                    var commandButton = commandButtonReference.Value;
+
+                    buttonControl.BackgroundImage = window.ImageLoader.CreateFromMappedImageReference(commandButton.ButtonImage);
+
+                    buttonControl.DisabledBackgroundImage = buttonControl.BackgroundImage?.WithGrayscale(true);
+
+                }
+            }
+
+            var rank8 = game.Scene3D.LocalPlayer.Template.PurchaseScienceCommandSetRank8;
+            for (int i = 0; i < 1; i++)
+            {
+                var buttonControl = window.Controls.FindControl("GeneralsExpPoints.wnd:ButtonRank8Number" + i);
+                if (rank1 != null && rank8.Value.Buttons.TryGetValue(i + 1, out var commandButtonReference))
                 {
                     var commandButton = commandButtonReference.Value;
 
