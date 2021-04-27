87cdd000c926e18b7812d3d298c4d4c3b780f7db
Stephan Vedder <stephan.vedder@gmail.com>
2021-04-27 07:16:43 +0200

    Avoid crashes in BFME

--- src/OpenSage.Game/Logic/Rank.cs
+++ src/OpenSage.Game/Logic/Rank.cs
@@ -19,10 +19,13 @@ namespace OpenSage.Logic
         public void SetRank(int id)
         {
             _currentRank = _rankTemplates.GetByInternalId(id);
-            foreach (var science in _currentRank.SciencesGranted)
+            if (_currentRank.SciencesGranted != null)
             {
-                _player.PurchaseScience(science.Value);
-                _player.SciencePurchasePoints += (uint)_currentRank.SciencePurchasePointsGranted;
+                foreach (var science in _currentRank.SciencesGranted)
+                {
+                    _player.PurchaseScience(science.Value);
+                    _player.SciencePurchasePoints += (uint) _currentRank.SciencePurchasePointsGranted;
+                }
             }
         }
 
@@ -35,6 +38,12 @@ namespace OpenSage.Logic
                 var firstRank = _rankTemplates.GetByIndex(0);
                 SetRank(firstRank.InternalId);
             }
+
+            if(_rankTemplates.Count == _currentRank.InternalId)
+            {
+                return;
+            }
+
             var nextRankId = _currentRank.InternalId + 1;
             var nextRank = _rankTemplates.GetByInternalId(nextRankId);
             if (_player.SkillPointsAvailable >= nextRank.SkillPointsNeeded)
