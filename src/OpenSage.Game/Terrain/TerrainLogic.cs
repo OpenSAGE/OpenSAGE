0060e9b0087d62de51faf407fa6e80fb92a21104
Tim Jones <tim@timjones.io>
2021-05-19 21:35:59 +0100

    Tweak TerrainLogic

--- src/OpenSage.Game/Terrain/TerrainLogic.cs
+++ src/OpenSage.Game/Terrain/TerrainLogic.cs
@@ -7,22 +7,18 @@ namespace OpenSage.Terrain
     {
         internal void Load(SaveFileReader reader)
         {
+            reader.ReadVersion(1);
+
             reader.ReadVersion(2);
 
             var unknown = reader.ReadInt32();
-            if (unknown != 2u)
+            if (unknown != 0)
             {
                 throw new InvalidDataException();
             }
 
             var unknown2 = reader.ReadInt32();
-            if (unknown2 != 0u)
-            {
-                throw new InvalidDataException();
-            }
-
-            var unknown3 = reader.ReadByte();
-            if (unknown3 != 0)
+            if (unknown2 != 0)
             {
                 throw new InvalidDataException();
             }
