24448ae4189e5993ebdf0684a133505d4e8e895b
Tim Jones <tim@timjones.io>
2021-05-27 21:37:58 +0100

    Cache cross-compiled shaders for target platform

--- src/OpenSage.Game/Graphics/Shaders/ShaderSet.cs
+++ src/OpenSage.Game/Graphics/Shaders/ShaderSet.cs
@@ -1,4 +1,8 @@
-﻿using OpenSage.Utilities.Extensions;
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using OpenSage.Utilities.Extensions;
 using Veldrid;
 using Veldrid.SPIRV;
 
@@ -6,7 +10,9 @@ namespace OpenSage.Graphics.Shaders
 {
     public sealed class ShaderSet : DisposableBase
     {
-        private static byte _nextId;
+        private const string EntryPoint = "main";
+
+        private static byte NextId;
 
         public readonly byte Id;
 
@@ -21,7 +27,7 @@ namespace OpenSage.Graphics.Shaders
         {
             GlobalResourceSetIndices = globalResourceSetIndices;
 
-            Id = _nextId++;
+            Id = NextId++;
 
 #if DEBUG
             const bool debug = true;
@@ -29,27 +35,15 @@ namespace OpenSage.Graphics.Shaders
             const bool debug = false;
 #endif
 
-            var assembly = typeof(ShaderSet).Assembly;
+            GetShaders(
+                graphicsDevice.ResourceFactory,
+                shaderName,
+                debug,
+                out var vertexShader,
+                out var fragmentShader);
 
-            byte[] ReadShader(string shaderType)
-            {
-                var bytecodeShaderName = $"OpenSage.Assets.Shaders.{shaderName}.{shaderType}.spv";
-                using (var shaderStream = assembly.GetManifestResourceStream(bytecodeShaderName))
-                {
-                    return shaderStream.ReadAllBytes();
-                }
-            }
-
-            var vsBytes = ReadShader("vert");
-            var fsBytes = ReadShader("frag");
-
-            var shaders = graphicsDevice.ResourceFactory.CreateFromSpirv(
-                new ShaderDescription(ShaderStages.Vertex, vsBytes, "main", debug),
-                new ShaderDescription(ShaderStages.Fragment, fsBytes, "main", debug),
-                new CrossCompileOptions());
-
-            var vertexShader = AddDisposable(shaders[0]);
-            var fragmentShader = AddDisposable(shaders[1]);
+            AddDisposable(vertexShader);
+            AddDisposable(fragmentShader);
 
             vertexShader.Name = $"{shaderName}.vert";
             fragmentShader.Name = $"{shaderName}.frag";
@@ -57,6 +51,173 @@ namespace OpenSage.Graphics.Shaders
             Description = new ShaderSetDescription(
                 vertexDescriptors,
                 new[] { vertexShader, fragmentShader });
+        }
+
+        private static void GetShaders(
+            ResourceFactory factory,
+            string shaderName,
+            bool debug,
+            out Shader vertexShader,
+            out Shader fragmentShader)
+        {
+            GetOrCreateCachedShaders(
+                factory,
+                shaderName,
+                debug,
+                out var vsBytes,
+                out var fsBytes);
+
+            var entryPoint = factory.BackendType == GraphicsBackend.Metal
+                ? $"{EntryPoint}0"
+                : EntryPoint;
+
+            vertexShader = factory.CreateShader(new ShaderDescription(ShaderStages.Vertex, vsBytes, entryPoint));
+            fragmentShader = factory.CreateShader(new ShaderDescription(ShaderStages.Fragment, fsBytes, entryPoint));
+        }
+
+        private static void GetOrCreateCachedShaders(
+            ResourceFactory factory,
+            string shaderName,
+            bool debug,
+            out byte[] vsBytes,
+            out byte[] fsBytes)
+        {
+            const string shaderCacheFolder = "ShaderCache";
+            var backendType = factory.BackendType;
+            var targetExtension = backendType.ToString().ToLowerInvariant();
+
+            if (!Directory.Exists(shaderCacheFolder))
+            {
+                Directory.CreateDirectory(shaderCacheFolder);
+            }
+
+            var vsSpvName = $"OpenSage.Assets.Shaders.{shaderName}.vert.spv";
+            var fsSpvName = $"OpenSage.Assets.Shaders.{shaderName}.frag.spv";
+
+            var vsSpvBytes = ReadSpvShader(vsSpvName);
+            var fsSpvBytes = ReadSpvShader(fsSpvName);
+
+            var vsSpvHash = GetShaderHash(vsSpvBytes);
+            var fsSpvHash = GetShaderHash(fsSpvBytes);
+
+            // Check that SPIR-V files on disk match what's in the assembly.
+            var vsCacheFilePath = Path.Combine(shaderCacheFolder, $"OpenSage.Assets.Shaders.{shaderName}.vert.{vsSpvHash}.{targetExtension}");
+            var fsCacheFilePath = Path.Combine(shaderCacheFolder, $"OpenSage.Assets.Shaders.{shaderName}.frag.{fsSpvHash}.{targetExtension}");
+
+            if (File.Exists(vsCacheFilePath) &&
+                File.Exists(fsCacheFilePath))
+            {
+                // Cache is valid - use it.
+                vsBytes = File.ReadAllBytes(vsCacheFilePath);
+                fsBytes = File.ReadAllBytes(fsCacheFilePath);
+                return;
+            }
+
+            // Cache is invalid or doesn't exist - do cross-compilation.
+
+            if (backendType == GraphicsBackend.Vulkan)
+            {
+                File.WriteAllBytes(vsCacheFilePath, vsSpvBytes);
+                File.WriteAllBytes(fsCacheFilePath, fsSpvBytes);
+                vsBytes = vsSpvBytes;
+                fsBytes = fsSpvBytes;
+                return;
+            }
+
+            var compilationTarget = GetCompilationTarget(backendType);
+            var compilationResult = SpirvCompilation.CompileVertexFragment(
+                vsSpvBytes,
+                fsSpvBytes,
+                compilationTarget,
+                new CrossCompileOptions());
+
+            switch (backendType)
+            {
+                case GraphicsBackend.Direct3D11:
+                    vsBytes = CompileHlsl(compilationResult.VertexShader, "vs_5_0", debug);
+                    fsBytes = CompileHlsl(compilationResult.FragmentShader, "ps_5_0", debug);
+                    break;
+
+                case GraphicsBackend.OpenGL:
+                case GraphicsBackend.OpenGLES:
+                    vsBytes = Encoding.ASCII.GetBytes(compilationResult.VertexShader);
+                    fsBytes = Encoding.ASCII.GetBytes(compilationResult.FragmentShader);
+                    break;
+
+                case GraphicsBackend.Metal:
+                    // TODO: Compile to IR.
+                    vsBytes = Encoding.UTF8.GetBytes(compilationResult.VertexShader);
+                    fsBytes = Encoding.UTF8.GetBytes(compilationResult.FragmentShader);
+                    break;
+
+                default:
+                    throw new InvalidOperationException();
+            }
+
+            File.WriteAllBytes(vsCacheFilePath, vsBytes);
+            File.WriteAllBytes(fsCacheFilePath, fsBytes);
+        }
+
+        private static byte[] CompileHlsl(string hlsl, string profile, bool debug)
+        {
+            var flags = debug
+                ? SharpDX.D3DCompiler.ShaderFlags.Debug
+                : SharpDX.D3DCompiler.ShaderFlags.OptimizationLevel3;
+            var compilationResult = SharpDX.D3DCompiler.ShaderBytecode.Compile(
+                hlsl,
+                EntryPoint,
+                profile,
+                flags);
+
+            if (compilationResult.HasErrors)
+            {
+                throw new Exception(compilationResult.Message);
+            }
+
+            return compilationResult.Bytecode.Data;
+        }
+
+        private static byte[] ReadSpvShader(string spvShaderName)
+        {
+            var assembly = typeof(ShaderSet).Assembly;
+            using var shaderStream = assembly.GetManifestResourceStream(spvShaderName);
+            return shaderStream.ReadAllBytes();
+        }
+
+        private static string GetShaderHash(byte[] shaderBytes)
+        {
+            using var sha1 = new SHA1Managed();
+
+            var hash = sha1.ComputeHash(shaderBytes);
+
+            var sb = new StringBuilder(hash.Length * 2);
+            foreach (var b in hash)
+            {
+                sb.Append(b.ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+
+        private static CrossCompileTarget GetCompilationTarget(GraphicsBackend backend)
+        {
+            switch (backend)
+            {
+                case GraphicsBackend.Direct3D11:
+                    return CrossCompileTarget.HLSL;
+
+                case GraphicsBackend.OpenGL:
+                    return CrossCompileTarget.GLSL;
+
+                case GraphicsBackend.Metal:
+                    return CrossCompileTarget.MSL;
+
+                case GraphicsBackend.OpenGLES:
+                    return CrossCompileTarget.ESSL;
+
+                default:
+                    throw new SpirvCompilationException($"Invalid GraphicsBackend: {backend}");
+            }
         }
     }
 
