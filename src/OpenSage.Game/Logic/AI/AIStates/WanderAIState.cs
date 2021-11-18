55f8f5f939ba2a3ddd0b9e5c61f89d8da193b85d
Tim Jones <tim@timjones.io>
2021-11-18 23:30:25 +0000

    Discovered name of state 40

---
+++ src/OpenSage.Game/Logic/AI/AIStates/WanderAIState.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using OpenSage.Data.Sav;
+
+namespace OpenSage.Logic.AI
+{
+    internal sealed class WanderAIState : AIState1
+    {
+        internal override void Load(SaveFileReader reader)
+        {
+            reader.ReadVersion(1);
+
+            base.Load(reader);
+
+            var unknownPos = reader.ReadVector3();
+
+            var unknownInt1 = reader.ReadUInt32();
+
+            var unknownInt2 = reader.ReadUInt32();
+            if (unknownInt2 != 0)
+            {
+                throw new InvalidDataException();
+            }
+        }
+    }
+}
