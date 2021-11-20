6a0a855ce05730a32cede785d02624d6d1922db3
Tim Jones <tim@timjones.io>
2021-11-20 00:00:15 +0000

    Attack area state

---
+++ src/OpenSage.Game/Logic/AI/AIStates/AttackAreaState.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using OpenSage.Data.Sav;
+
+namespace OpenSage.Logic.AI.AIStates
+{
+    internal sealed class AttackAreaState : State
+    {
+        private readonly AttackAreaAIStateMachine _stateMachine;
+
+        public AttackAreaState()
+        {
+            _stateMachine = new AttackAreaAIStateMachine();
+        }
+
+        internal override void Load(SaveFileReader reader)
+        {
+            reader.ReadVersion(1);
+
+            var unknownBool1 = reader.ReadBoolean();
+            if (!unknownBool1)
+            {
+                throw new InvalidDataException();
+            }
+
+            _stateMachine.Load(reader);
+
+            var unknownInt = reader.ReadUInt32();
+        }
+    }
+
+    internal sealed class AttackAreaAIStateMachine : StateMachineBase
+    {
+        public AttackAreaAIStateMachine()
+        {
+            AddState(0, new IdleState());
+            AddState(10, new AttackState());
+        }
+
+        internal override void Load(SaveFileReader reader)
+        {
+            reader.ReadVersion(1);
+
+            base.Load(reader);
+        }
+    }
+}
---
+++ src/OpenSage.Game/Logic/AI/AIStates/DeadState.cs
@@ -0,0 +1,12 @@
+﻿using OpenSage.Data.Sav;
+
+namespace OpenSage.Logic.AI.AIStates
+{
+    internal sealed class DeadState : State
+    {
+        internal override void Load(SaveFileReader reader)
+        {
+            reader.ReadVersion(1);
+        }
+    }
+}
