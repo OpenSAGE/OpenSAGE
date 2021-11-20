baa92feb1da79a172c62769a389b6ad8bbec55b4
Tim Jones <tim@timjones.io>
2021-11-20 23:05:11 +0000

    Enter / exit container state

---
+++ src/OpenSage.Game/Logic/AI/AIStates/ExitContainerState.cs
@@ -0,0 +1,14 @@
+﻿using OpenSage.Data.Sav;
+
+namespace OpenSage.Logic.AI.AIStates
+{
+    internal sealed class ExitContainerState : State
+    {
+        internal override void Load(SaveFileReader reader)
+        {
+            reader.ReadVersion(1);
+
+            var containerObjectId = reader.ReadObjectID();
+        }
+    }
+}
