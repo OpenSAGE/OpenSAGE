74a4d412a04de64b3a0d7a5fe7715113b33ce8ff
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2022-01-09 11:45:44 +0100

    Create Drawables without GameObjects

--- src/OpenSage.Game/Logic/Object/Draw/W3dTracerDraw.cs
+++ src/OpenSage.Game/Logic/Object/Draw/W3dTracerDraw.cs
@@ -1,7 +1,68 @@
-﻿using OpenSage.Data.Ini;
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using OpenSage.Client;
+using OpenSage.Data.Ini;
+using OpenSage.Graphics;
+using OpenSage.Graphics.Cameras;
+using OpenSage.Graphics.Rendering;
+using OpenSage.Graphics.Shaders;
+using OpenSage.Mathematics;
 
 namespace OpenSage.Logic.Object
 {
+    public class W3dTracerDraw : DrawModule
+    {
+        private readonly W3dTracerDrawModuleData _data;
+
+        public override IEnumerable<BitArray<ModelConditionFlag>> ModelConditionStates => Array.Empty<BitArray<ModelConditionFlag>>();
+
+        internal W3dTracerDraw(W3dTracerDrawModuleData data, GameContext context)
+        {
+            _data = data;
+        }
+
+        internal override void Update(in TimeInterval gameTime)
+        {
+            // TODO: implement
+        }
+
+        internal override void BuildRenderList(RenderList renderList, Camera camera, bool castsShadow, MeshShaderResources.RenderItemConstantsPS renderItemConstantsPS, Dictionary<string, bool> shownSubObjects = null, Dictionary<string, bool> hiddenSubObjects = null)
+        {
+            // TODO: Implement
+        }
+
+        internal override void Load(StatePersister reader)
+        {
+            reader.PersistVersion(1);
+
+            reader.BeginObject("Base");
+            base.Load(reader);
+            reader.EndObject();
+        }
+
+        internal override string GetWeaponFireFXBone(WeaponSlot slot)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override string GetWeaponLaunchBone(WeaponSlot slot)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        internal override void SetWorldMatrix(in Matrix4x4 worldMatrix)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override (ModelInstance, ModelBone) FindBone(string boneName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     /// <summary>
     /// Requires object to be KindOf = DRAWABLE_ONLY.
     /// </summary>
@@ -10,5 +71,10 @@ namespace OpenSage.Logic.Object
         internal static W3dTracerDrawModuleData Parse(IniParser parser) => parser.ParseBlock(FieldParseTable);
 
         private static readonly IniParseTable<W3dTracerDrawModuleData> FieldParseTable = new IniParseTable<W3dTracerDrawModuleData>();
+
+        internal override DrawModule CreateDrawModule(Drawable drawable, GameContext context)
+        {
+            return new W3dTracerDraw(this, context);
+        }
     }
 }
