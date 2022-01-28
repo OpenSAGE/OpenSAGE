0b80d8a5751338bca80270c62d10539f5adcb531
Tim Jones <tim@timjones.io>
2022-01-28 19:42:04 +0000

    Rendering experiments

--- src/OpenSage.Rendering/RenderScene.cs
+++ src/OpenSage.Rendering/RenderScene.cs
@@ -1,35 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using OpenSage.Mathematics;
 using Veldrid;
 
 namespace OpenSage.Rendering;
 
 public sealed class RenderScene
 {
-    private readonly List<RenderableObject> _renderables = new();
+    private readonly List<RenderObject> _renderObjects = new();
 
     private readonly RenderList _forwardPassList = new();
     private readonly RenderList _shadowPassList = new();
 
-    public void AddObject(RenderableObject renderable)
+    public void AddObject(RenderObject renderObject)
     {
-        _renderables.Add(renderable);
+        _renderObjects.Add(renderObject);
+
+        _forwardPassList.AddObject(renderObject, renderObject.MaterialPass.ForwardPass);
+
+        if (renderObject.MaterialPass.ShadowPass != null)
+        {
+            _shadowPassList.AddObject(renderObject, renderObject.MaterialPass.ShadowPass);
+        }
 
         // TODO: Extract child objects and store in appropriate render buckets (opaque / transparent / etc.)
     }
+
+    public void RemoveObject(RenderObject renderObject)
+    {
+        _renderObjects.Remove(renderObject);
+
+        _forwardPassList.RemoveObject(renderObject, renderObject.MaterialPass.ForwardPass);
+
+        if (renderObject.MaterialPass.ShadowPass != null)
+        {
+            _shadowPassList.RemoveObject(renderObject, renderObject.MaterialPass.ShadowPass);
+        }
+    }
+
+    public void Render()
+    {
+        // TODO: Draw shadow map using _shadowPassList.
+        //DoShadowPass();
+
+        DoForwardPass();
+    }
+
+    private void DoForwardPass()
+    {
+        //_forwardPassList.Cull();
+    }
 }
 
-public abstract class RenderableObject
+public abstract class RenderObject : DisposableBase
 {
-    public readonly List<RenderableObject> ChildObjects = new();
+    public abstract MaterialPass MaterialPass { get; }
 
-    public abstract SurfaceType SurfaceType { get; }
+    //public readonly List<RenderableObject> ChildObjects = new();
 
-    public abstract ulong RenderKey { get; } // ShaderSet, Pipeline, Material, SortPriority
+    public abstract AxisAlignedBoundingBox BoundingBox { get; }
+
+    //public abstract Material Material { get; }
+
+    //public abstract SurfaceType SurfaceType { get; }
+
+    //public abstract ulong RenderKey { get; } // ShaderSet, Pipeline, Material, SortPriority
 
     //public abstract int SortPriority { get; } // Needs to be lower for Road than for ParticleSystem
 
-    public abstract bool CastsShadow { get; }
+    //public abstract bool CastsShadow { get; }
+
+    public abstract void Render();
 }
 
 public readonly struct RenderKey
@@ -49,34 +89,6 @@ public readonly struct RenderKey
     }
 }
 
-//public sealed class RenderContext
-//{
-//}
-
-public sealed class Material : DisposableBase
-{
-    public readonly byte Id;
-    public readonly ShaderSet ShaderSet;
-    public readonly Pipeline Pipeline;
-    public readonly ResourceSet MaterialResourceSet;
-
-    public Material(
-        ShaderSet shaderSet,
-        Pipeline pipeline,
-        ResourceSet materialResourceSet)
-    {
-        Id = shaderSet.GetNextMaterialId();
-
-        ShaderSet = shaderSet;
-        Pipeline = pipeline;
-        MaterialResourceSet = materialResourceSet;
-    }
-
-    public ulong RenderKey { get; } // TODO
-}
-
-public sealed record MaterialPass(Material ForwardPass, Material ShadowPass);
-
 public enum SurfaceType
 {
     Opaque,
@@ -94,6 +106,23 @@ public enum RenderBucketType
 
 internal sealed class RenderList
 {
-    public readonly List<RenderableObject> Opaque = new();
-    public readonly List<RenderableObject> Transparent = new();
+    public readonly List<(RenderObject, Material)> AllObjects = new();
+
+    public readonly List<(RenderObject, Material)> CulledObjects = new();
+
+    public void AddObject(RenderObject renderObject, Material material)
+    {
+        AllObjects.Add((renderObject, material));
+
+        // TODO: Use something better like https://www.jacksondunstan.com/articles/3189
+        AllObjects.Sort((x, y) => x.Item2.RenderKey.CompareTo(y.Item2.RenderKey));
+    }
+
+    public void RemoveObject(RenderObject renderObject, Material material)
+    {
+        // TODO: Do something faster.
+        var index = AllObjects.IndexOf((renderObject, material));
+
+        AllObjects.RemoveAt(index);
+    }
 }
