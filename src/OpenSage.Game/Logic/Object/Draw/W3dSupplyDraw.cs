2960b44f63cc652edd238b420f0526cad8f437c6
Charlie <30303272+charliefoxtwo@users.noreply.github.com>
2024-01-11 17:14:50 -0800

    Hide supply box bones as supplies are depleted

--- src/OpenSage.Game/Logic/Object/Draw/W3dSupplyDraw.cs
+++ src/OpenSage.Game/Logic/Object/Draw/W3dSupplyDraw.cs
@@ -1,13 +1,39 @@
-﻿using OpenSage.Client;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenSage.Client;
 using OpenSage.Data.Ini;
 
 namespace OpenSage.Logic.Object
 {
     public sealed class W3dSupplyDraw : W3dModelDraw
     {
+        private readonly Dictionary<string, int> _boxBoneMap;
+        private readonly string _bonePrefix;
+
         internal W3dSupplyDraw(W3dSupplyDrawModuleData data, Drawable drawable, GameContext context)
             : base(data, drawable, context)
         {
+            _bonePrefix = data.SupplyBonePrefix;
+            _boxBoneMap = string.IsNullOrEmpty(data.SupplyBonePrefix)
+                ? new Dictionary<string, int>()
+                : ActiveModelInstance.Model.BoneHierarchy.Bones.Where(b => b.Name.StartsWith(data.SupplyBonePrefix))
+                    .ToDictionary(b => b.Name, b => b.Index);
+        }
+
+        public void SetBoxesRemaining(float boxPercentage)
+        {
+            var totalBones = _boxBoneMap.Count;
+
+            // use ceiling so we don't hide all the boxes when there are still supplies left
+            var boxesRemaining = (int) Math.Ceiling(totalBones * boxPercentage);
+            for (var i = totalBones; i > boxesRemaining; i--)
+            {
+                if (_boxBoneMap.TryGetValue($"{_bonePrefix}{i:00}", out var boneIndex))
+                {
+                    ActiveModelInstance.BoneVisibilities[boneIndex] = false;
+                }
+            }
         }
 
         internal override void Load(StatePersister reader)
