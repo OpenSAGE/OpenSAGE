c44271454314baa56b974d31df03b664f03faf38
Charlie <30303272+charliefoxtwo@users.noreply.github.com>
2024-01-21 12:06:24 -0800

    Don't draw veterancy for units in containers

--- src/OpenSage.Game/Scene3D.cs
+++ src/OpenSage.Game/Scene3D.cs
@@ -624,6 +624,11 @@ namespace OpenSage
             {
                 foreach (var obj in GameObjects.Items)
                 {
+                    if (obj.Hidden)
+                    {
+                        continue;
+                    }
+
                     if (obj.IsSelected || LocalPlayer.HoveredUnit == obj)
                     {
                         DrawHealthBox(obj);
