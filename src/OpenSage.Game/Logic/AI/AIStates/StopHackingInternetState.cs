95132ea1543234cb07d68b3334c988efdc31282a
Charlie <30303272+charliefoxtwo@users.noreply.github.com>
2024-02-28 18:15:29 -0800

    Implement HackInternetAIUpdate

---
+++ src/OpenSage.Game/Logic/AI/AIStates/StopHackingInternetState.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenSage.Logic.AI.AIStates
+{
+    internal sealed class StopHackingInternetState : State
+    {
+        public uint FramesUntilFinishedPacking;
+
+        public override void Persist(StatePersister reader)
+        {
+            reader.PersistVersion(1);
+
+            reader.PersistUInt32(ref FramesUntilFinishedPacking);
+        }
+    }
+}
