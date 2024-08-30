858cf667ec9da12965f5a5fe3cc927e22af8457a
Tim Jones <tim@timjones.io>
2024-08-30 09:55:23 +0800

    Add comments to move state fields

--- src/OpenSage.Game/Logic/AI/AIStates/MoveTowardsState.cs
+++ src/OpenSage.Game/Logic/AI/AIStates/MoveTowardsState.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using OpenSage.Logic.Object;
 
 namespace OpenSage.Logic.AI.AIStates
 {
@@ -8,7 +9,7 @@ namespace OpenSage.Logic.AI.AIStates
         private uint _unknownInt1;
         private bool _unknownBool1;
         private Vector3 _unknownPosition2;
-        private uint _unknownInt2;
+        private LogicFrame _unknownFrame;
         private uint _unknownInt3;
         private bool _unknownBool2;
 
@@ -18,9 +19,9 @@ namespace OpenSage.Logic.AI.AIStates
 
             reader.PersistVector3(ref _targetPosition);
             reader.PersistUInt32(ref _unknownInt1);
-            reader.PersistBoolean(ref _unknownBool1);
-            reader.PersistVector3(ref _unknownPosition2);
-            reader.PersistUInt32(ref _unknownInt2);
+            reader.PersistBoolean(ref _unknownBool1); // True until frame after path is ready in AIUpdate, then it's false
+            reader.PersistVector3(ref _unknownPosition2); // Empty until frame after path is ready in AIUpdate, then it's the original path destination (original because the one in _targetPosition will have been modified to be at the centre of a terrain cell)
+            reader.PersistLogicFrame(ref _unknownFrame); // 0 until frame X after path is ready in AIUpdate, then it's frame X
             reader.PersistUInt32(ref _unknownInt3);
             reader.PersistBoolean(ref _unknownBool2);
         }
