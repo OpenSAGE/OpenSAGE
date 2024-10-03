acaa8476ecf2290c03eb6f641aa72848ea17b15f
Valerii Myronov <valeriy@hey.com>
2024-10-03 21:37:49 +0100

    Added some tests for changed code

---
+++ src/OpenSage.Game.Tests/GameWindowTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using OpenSage.Input;
+using Veldrid.Sdl2;
+using Xunit;
+
+namespace OpenSage.Tests;
+
+public class GameWindowTests : StatePersisterTest
+{
+    [Fact]
+    public void GameWindow_Create()
+    {
+        var gameWindow = new GameWindow("Generals", 0, 0, 1024, 768, false);
+
+        Assert.NotNull(gameWindow);
+    }
+
+    [Fact]
+    public void GameWindow_HandleMouseWheel_When_NotZeroDelta()
+    {
+        var gameWindow = new GameWindow("Generals", 0, 0, 1024, 768, false);
+
+        var methodInfo = typeof(GameWindow).GetMethod("HandleMouseWheel", BindingFlags.NonPublic | BindingFlags.Instance);
+        methodInfo.Invoke(gameWindow, [buildMouseWheelEventArgs(x: 100, y: 50, wheelDelta: 5)]);
+
+        Assert.Single(gameWindow.MessageQueue);
+        var inputMessage = gameWindow.MessageQueue.Dequeue();
+        Assert.Equal(InputMessageType.MouseWheel, inputMessage.MessageType);
+        Assert.Equal(500, inputMessage.Value.ScrollWheel); // Mouse wheel delta is multiplied by 100
+        Assert.Equal(100, inputMessage.Value.MousePosition.X);
+        Assert.Equal(50, inputMessage.Value.MousePosition.Y);
+    }
+
+    [Fact]
+    public void GameWindow_HandleMouseWheel_When_ZeroDelta()
+    {
+        var gameWindow = new GameWindow("Generals", 0, 0, 1024, 768, false);
+
+        var methodInfo = typeof(GameWindow).GetMethod("HandleMouseWheel", BindingFlags.NonPublic | BindingFlags.Instance);
+        methodInfo.Invoke(gameWindow, [buildMouseWheelEventArgs(x: 100, y: 50, wheelDelta: 0)]);
+
+        Assert.Empty(gameWindow.MessageQueue);
+    }
+
+    private static MouseWheelEventArgs buildMouseWheelEventArgs(int x, int y, int wheelDelta)
+    {
+        var mouseState = new MouseState(x, y, false, false, false, false, false, false, false, false, false, false, false, false, false);
+        return new MouseWheelEventArgs(mouseState,wheelDelta);
+    }
+}
---
+++ src/OpenSage.Game.Tests/Gui/Wnd/WndInputMessageHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using OpenSage.Input;
+using OpenSage.Gui.Wnd;
+using OpenSage.Mathematics;
+using OpenSage.Data.Wnd;
+using OpenSage.Gui.Wnd.Controls;
+using Xunit;
+
+namespace OpenSage.Tests.Gui.Wnd
+{
+    public class WndInputMessageHandlerTests(GameFixture fixture) : IClassFixture<GameFixture>
+    {
+        private readonly GameFixture _fixture = fixture;
+
+        [Fact]
+        public void HandleMessage_When_MouseWheelEventWithNoControlUnderMouse()
+        {
+            var game = _fixture.GetGame(SageGame.CncGenerals);
+            var windowManager = new WndWindowManager(game);
+            var handler = new WndInputMessageHandler(windowManager, game);
+
+            var inputMessage = InputMessage.CreateMouseWheel(500, new Point2D(100, 50));
+            var result = handler.HandleMessage(inputMessage);
+            Assert.Equal(InputMessageResult.NotHandled, result);
+        }
+    }
+}
