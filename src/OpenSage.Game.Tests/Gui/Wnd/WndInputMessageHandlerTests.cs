4c330606ba6edecce43054694bd03563f960cf98
Valerii Myronov <valeriy@hey.com>
2024-10-04 09:14:54 +0100

    Skip test requiring game files (similar to SaveFileTests)

--- src/OpenSage.Game.Tests/Gui/Wnd/WndInputMessageHandlerTests.cs
+++ src/OpenSage.Game.Tests/Gui/Wnd/WndInputMessageHandlerTests.cs
@@ -9,9 +9,13 @@ namespace OpenSage.Tests.Gui.Wnd
 {
     public class WndInputMessageHandlerTests(GameFixture fixture) : IClassFixture<GameFixture>
     {
+        // swap to the null skip message in order to run locally
+        private const string Skip = "game files required";
+        // private const string? Skip = null;
+
         private readonly GameFixture _fixture = fixture;
 
-        [Fact]
+        [Fact(Skip = Skip)]
         public void HandleMessage_When_MouseWheelEventWithNoControlUnderMouse()
         {
             var game = _fixture.GetGame(SageGame.CncGenerals);
