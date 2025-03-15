2ed82e0b4cbd1d6adb4b6656c16f97d5c85465e8
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2025-03-15 12:22:28 +0100

    Port path

---
+++ src/OpenSage.Game/Logic/AI/AIPathfind.PathNode.cs
@@ -0,0 +1,166 @@
+﻿using System.Numerics;
+using OpenSage.Logic.Object;
+
+namespace OpenSage.Logic.AI;
+
+public partial class AIPathfind
+{
+    class PathNode
+    {
+        /// <summary>
+        ///  Used in Path::xfer() to save & recreate the path list.
+        /// </summary>
+        public int _id = -1;
+
+        private PathNode? _nextOptimized;
+
+        /// <summary>
+        /// if <see cref="NextOptimized"/> is nonnull, the dist to it.
+        /// </summary>
+        private float _nextOptimizedDistance2D;
+
+        /// <summary>
+        /// if <see cref="NextOptimized"/> is nonnull, normalized dir vec towards it.
+        /// </summary>
+        private Vector2 _nextOptimizedDirectionNormalized2D;
+
+        /// <summary>
+        /// position of node in space
+        /// </summary>
+        public Vector3 Position { get; set; }
+
+        /// <summary>
+        /// next node in the path
+        /// </summary>
+        public PathNode? Next { get; set; }
+
+        /// <summary>
+        ///  previous node in the path
+        /// </summary>
+        public PathNode? Previous { get; set; }
+
+        /// <summary>
+        /// next node in the optimized path
+        /// </summary>
+        public PathNode? NextOptimized
+        {
+            get => _nextOptimized;
+            set
+            {
+                _nextOptimized = value;
+                if (value is not null)
+                {
+                    _nextOptimizedDirectionNormalized2D = new Vector2(value.Position.X - Position.X, value.Position.Y - Position.Y);
+                    _nextOptimizedDistance2D = _nextOptimizedDirectionNormalized2D.Length();
+                    if (_nextOptimizedDistance2D == 0.0f)
+                    {
+                        _nextOptimizedDistance2D = 0.01f;
+                    }
+
+                    _nextOptimizedDirectionNormalized2D /= _nextOptimizedDistance2D;
+                }
+                else
+                {
+                    _nextOptimizedDistance2D = 0.0f;
+                }
+            }
+        }
+
+        public PathNode? GetNextOptimized(out Vector2 direction, out float distance)
+        {
+            direction = _nextOptimizedDirectionNormalized2D;
+            distance = _nextOptimizedDistance2D;
+
+            return _nextOptimized;
+        }
+
+        /// <summary>
+        /// Layer for this section.
+        /// </summary>
+        public PathfindLayerType Layer { get; set; }
+
+        /// <summary>
+        /// True if this cell can be optimized out.
+        /// </summary>
+        public bool CanOptimize { get; set; }
+
+
+        /// <summary>
+        ///  given a list, prepend this node, return new list
+        /// </summary>
+        public PathNode PrependToList(PathNode? list)
+        {
+            Next = list;
+            if (list is not null)
+            {
+                list.Previous = this;
+            }
+
+            Previous = null;
+            return this;
+        }
+
+        /// <summary>
+        /// given a list, append this node, return new list. slow implementation.
+        /// TODO optimize this
+        /// </summary>
+        public PathNode AppendToList(PathNode? list)
+        {
+            if (list is null)
+            {
+                Next = null;
+                Previous = null;
+                return this;
+            }
+
+            PathNode tail;
+            for (tail = list; tail.Next is not null; tail = tail.Next)
+                ;
+
+            tail.Next = this;
+            Previous = tail;
+            Next = null;
+
+            return list;
+        }
+
+        /// <summary>
+        /// given a node, append new node to this.
+        /// </summary>
+        public void Append(PathNode newNode)
+        {
+            newNode.Next = Next;
+            newNode.Previous = this;
+            if (newNode.Next is not null)
+            {
+                newNode.Next.Previous = newNode;
+            }
+
+            Next = newNode;
+        }
+
+        /// <summary>
+        /// Compute direction vector to next node
+        /// </summary>
+        public Vector3 ComputeDirectionVector()
+        {
+            if (Next is null)
+            {
+                if (Previous is null)
+                {
+                    // only one node on whole path - no direction
+                    return Vector3.Zero;
+                }
+                else
+                {
+                    // tail node - continue prior direction
+                    return Previous.ComputeDirectionVector();
+                }
+            }
+            else
+            {
+                return Next.Position - Position;
+            }
+        }
+    }
+}
--- src/OpenSage.Game/Logic/AI/AIPathfind.cs
+++ src/OpenSage.Game/Logic/AI/AIPathfind.cs
@@ -1,10 +1,12 @@
-﻿using System.Numerics;
-using OpenSage.Logic.Object;
-using OpenSage.Navigation;
+﻿using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+#nullable enable
 
 namespace OpenSage.Logic.AI;
 
-public class AIPathfind
+public partial class AIPathfind
 {
     /// <summary>
     /// How close is close enough when moving.
@@ -14,162 +16,18 @@ public class AIPathfind
     public const float PathfindCellSizeF = 10.0f;
     public const int PathfindQueueLength = 512;
 
-    class PathNode
+    /// <summary>
+    /// Fits in 4 bits for now
+    /// </summary>
+    public const int MaxWallPieces = 128;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsReallyClose(Vector3 a, Vector3 b)
     {
-        /// <summary>
-        ///  Used in Path::xfer() to save & recreate the path list.
-        /// </summary>
-        public int _id = -1;
-
-        private PathNode? _nextOptimized;
-
-        /// <summary>
-        /// if <see cref="NextOptimized"/> is nonnull, the dist to it.
-        /// </summary>
-        private float _nextOptimizedDistance2D;
-
-        /// <summary>
-        /// if <see cref="NextOptimized"/> is nonnull, normalized dir vec towards it.
-        /// </summary>
-        private Vector2 _nextOptimizedDirectionNormalized2D;
-
-        /// <summary>
-        /// position of node in space
-        /// </summary>
-        public Vector3 Position { get; set; }
-
-        /// <summary>
-        /// next node in the path
-        /// </summary>
-        public PathNode Next { get; set; }
-
-        /// <summary>
-        ///  previous node in the path
-        /// </summary>
-        public PathNode Previous { get; set; }
-
-        /// <summary>
-        /// next node in the optimized path
-        /// </summary>
-        public PathNode NextOptimized
-        {
-            get => _nextOptimized;
-            set
-            {
-                _nextOptimized = value;
-                if (value is not null)
-                {
-                    _nextOptimizedDirectionNormalized2D = new Vector2(value.Position.X - Position.X, value.Position.Y - Position.Y);
-                    _nextOptimizedDistance2D = _nextOptimizedDirectionNormalized2D.Length();
-                    if (_nextOptimizedDistance2D == 0.0f)
-                    {
-                        _nextOptimizedDistance2D = 0.01f;
-                    }
-
-                    _nextOptimizedDirectionNormalized2D /= _nextOptimizedDistance2D;
-                }
-                else
-                {
-                    _nextOptimizedDistance2D = 0.0f;
-                }
-            }
-        }
-
-        public PathNode? GetNextOptimized(out Vector2 direction, out float distance)
-        {
-            direction = _nextOptimizedDirectionNormalized2D;
-            distance = _nextOptimizedDistance2D;
-
-            return _nextOptimized;
-        }
-
-        /// <summary>
-        /// Layer for this section.
-        /// </summary>
-        public PathfindLayerType Layer { get; set; }
-
-        /// <summary>
-        /// True if this cell can be optimized out.
-        /// </summary>
-        public bool CanOptimize { get; set; }
-
-
-        /// <summary>
-        ///  given a list, prepend this node, return new list
-        /// </summary>
-        PathNode PrependToList(PathNode? list)
-        {
-            Next = list;
-            if (list is not null)
-            {
-                list.Previous = this;
-            }
-
-            Previous = null;
-            return this;
-        }
-
-        /// <summary>
-        /// given a list, append this node, return new list. slow implementation.
-        /// @todo optimize this
-        /// </summary>
-        PathNode AppendToList(PathNode list)
-        {
-            if (list is null)
-            {
-                Next = null;
-                Previous = null;
-                return this;
-            }
-
-            PathNode tail;
-            for (tail = list; tail.Next is not null; tail = tail.Next)
-                ;
-
-            tail.Next = this;
-            Previous = tail;
-            Next = null;
-
-            return list;
-        }
-
-        /// <summary>
-        /// given a node, append new node to this.
-        /// </summary>
-        void Append(PathNode newNode)
-        {
-            newNode.Next = Next;
-            newNode.Previous = this;
-            if (newNode.Next is not null)
-            {
-                newNode.Next.Previous = newNode;
-            }
-
-            Next = newNode;
-        }
-
-        /// <summary>
-        /// Compute direction vector to next node
-        /// </summary>
-        public Vector3 ComputeDirectionVector()
-        {
-            if (Next is null)
-            {
-                if (Previous is null)
-                {
-                    // only one node on whole path - no direction
-                    return Vector3.Zero;
-                }
-                else
-                {
-                    // tail node - continue prior direction
-                    return Previous.ComputeDirectionVector();
-                }
-            }
-            else
-            {
-                return Next.Position - Position;
-            }
-        }
+	    const float closeEnough = 0.1f;
+	    return 
+		    MathF.Abs(a.X - b.X) <= closeEnough &&
+		    MathF.Abs(a.Y - b.Y) <= closeEnough &&
+            MathF.Abs(a.Z - b.Z) <= closeEnough;
     }
 }
