d134e1df5b55824a468edc04a1f08e009ce7735f
Tim Jones <tim@timjones.io>
2025-03-20 23:51:42 +0000

    Add tests

---
+++ src/OpenSage.Game.Tests/Logic/Object/Body/InactiveBodyTests.cs
@@ -0,0 +1,50 @@
+﻿using OpenSage.Logic.Object;
+using Xunit;
+
+namespace OpenSage.Tests.Logic.Object.Body;
+
+public class InactiveBodyTests : ModuleTest
+{
+    [Fact]
+    public void InactiveBodyHasZeroHealth()
+    {
+        var body = CreateBody();
+
+        Assert.Equal(0.0f, body.Health);
+        Assert.Equal(0.0f, body.MaxHealth);
+        Assert.Equal(0.0f, body.InitialHealth);
+        Assert.Equal(0.0f, body.PreviousHealth);
+    }
+
+    [Fact]
+    public void InactiveBodyIgnoresNonUnresistableDamage()
+    {
+        var body = CreateBody();
+
+        var damageOutput = body.AttemptDamage(new DamageInfoInput
+        {
+            DamageType = DamageType.Explosion
+        });
+
+        Assert.True(damageOutput.NoEffect);
+    }
+
+    [Fact]
+    public void InactiveBodyIgnoresHealing()
+    {
+        var body = CreateBody();
+
+        var damageOutput = body.AttemptHealing(new DamageInfoInput
+        {
+            DamageType = DamageType.Healing
+        });
+
+        Assert.True(damageOutput.NoEffect);
+    }
+
+    private InactiveBody CreateBody()
+    {
+        var gameObject = new GameObject(new ObjectDefinition(), ZeroHour.GameEngine, null);
+        return new InactiveBody(gameObject, ZeroHour.GameEngine);
+    }
+}
