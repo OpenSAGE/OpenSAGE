94e6a6da5e06ae9d9b231fac22c7f9e6b7343a57
Paavo Huhtala <paavo.huhtala@gmail.com>
2025-03-25 22:06:06 +0200

    Add ObjectId struct

---
+++ src/OpenSage.Game/Logic/Object/ObjectId.cs
@@ -0,0 +1,22 @@
+﻿
+namespace OpenSage.Logic.Object;
+
+public readonly record struct ObjectId(uint Index)
+{
+    /// <summary>
+    /// An object ID which is guaranteed to be invalid (0).
+    /// This is commonly used as a sentinel value to indicate that a reference to a game object is non-existent or invalid.
+    /// </summary>
+    public static readonly ObjectId Invalid = new(0);
+
+    /// <summary>
+    /// Indicates whether this object ID is potentially valid (non-zero).
+    /// Whether or not an object ID is actually valid (i.e. corresponds to an existing object) depends on the current state of the game.
+    /// </summary>
+    public readonly bool IsValid => Index != 0;
+
+    /// <summary>
+    /// Indicates whether this object ID is invalid (0).
+    /// </summary>
+    public readonly bool IsInvalid => Index == 0;
+}
